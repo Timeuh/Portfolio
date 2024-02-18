af23d5fd37c8019cd9df6efa619e4564f09d400d
Timeuh <timeuuh@gmail.com>
2024-02-18 21:34:13 +0100

    :wrench: create custom font export file

---
+++ app/assets/fonts/fonts.ts
@@ -0,0 +1,13 @@
+import localFont from 'next/dist/compiled/@next/font/dist/local';
+
+export const Rubik = localFont({
+  src: 'Rubik/Rubik-VariableFont_wght.ttf',
+  display: 'swap',
+  variable: '--font-rubik',
+});
+
+export const BricolageGroteske = localFont({
+  src: 'Bricolage_Groteske/BricolageGroteske-VariableFont_wght.ttf',
+  display: 'swap',
+  variable: '--font-bricolage',
+});
