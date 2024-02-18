ce6cada098742e8ed32e63c78d13dd1058827556
Timothée Brindejonc <96261549+Timeuh@users.noreply.github.com>
2024-02-18 21:47:54 +0100

    Config/plugins (#1)
    * :wrench: install panda css
    
    * :wrench: add react-query
    
    * :wrench: add vinejs for data validation
    
    * :wrench: remove boilerplate images
    
    * :wrench: add prisma orm
    
    * :wrench: init prisma config
    
    * :wrench: remove prettier plugin for tailwind css
    
    * :wrench: remove boilerplate code
    
    * :wrench: add docker compose configuration for database
    
    * :wrench: use turbopack with dev script
    
    * :wrench: ignore prisma migrations
    
    * :wrench: create prisma database schema
    
    * :wrench: add eslint-config-next plugin
    
    * :wrench: remove eslint-config-prettier plugin
    
    * :lipstick: add custom colors to panda config
    
    * :wrench: add custom fonts
    
    * :wrench: create custom font export file
    
    * :wrench: add assets path alias
    
    * :recycle: import custom fonts + change page metadata
    
    * :lipstick: use right localFont from next + fix bricolage grotesque font file name for import
    
    * :lipstick: add fonts to config

---
+++ app/assets/fonts/fonts.ts
@@ -0,0 +1,13 @@
+import localFont from 'next/font/local';
+
+export const Rubik = localFont({
+  src: 'Rubik/Rubik-VariableFont_wght.ttf',
+  display: 'swap',
+  variable: '--font-rubik',
+});
+
+export const BricolageGroteske = localFont({
+  src: 'Bricolage_Groteske/BricolageGrotesque-VariableFont_wght.ttf',
+  display: 'swap',
+  variable: '--font-bricolage',
+});
