71dddc760df41c0768b94718fd3b8b124546b778
Timeuh <timeuuh@gmail.com>
2024-02-23 20:06:43 +0100

    :safety_vest: create admin schemas

---
+++ app/vine/api/admin/admin.schema.ts
@@ -0,0 +1,40 @@
+import vine from '@vinejs/vine';
+import {Infer} from '@vinejs/vine/types';
+import {linksSchema} from '@validation/api/links/links.schema';
+
+/* -------------------------------------------------------------------------- */
+/*                                  Schemas                                   */
+/* -------------------------------------------------------------------------- */
+
+// admin returned by api
+const adminSchema = vine.object({
+  email: vine.string().email(),
+  id: vine.number(),
+  links: linksSchema.clone(),
+});
+
+// admin when deleted
+const adminWhenDeletedSchema = vine.object({
+  email: vine.string().email(),
+  id: vine.number(),
+});
+
+/* -------------------------------------------------------------------------- */
+/*                                   Types                                    */
+/* -------------------------------------------------------------------------- */
+
+// type for admin returned by api
+export type Admin = Infer<typeof adminSchema>;
+
+// type for admin when deleted
+export type AdminWhenDeleted = Infer<typeof adminWhenDeletedSchema>;
+
+/* -------------------------------------------------------------------------- */
+/*                                 Validators                                 */
+/* -------------------------------------------------------------------------- */
+
+// validator for admin returned by api
+export const adminValidator = vine.compile(adminSchema);
+
+// validator for admin when deleted
+export const adminWhenDeletedValidator = vine.compile(adminWhenDeletedSchema);
