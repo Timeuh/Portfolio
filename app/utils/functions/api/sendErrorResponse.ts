22d2c9d9d46f6fc625415bd9be03f63439891155
Timeuh <timeuuh@gmail.com>
2024-02-24 19:05:37 +0100

    :sparkles: handle schema validation error

--- app/utils/functions/api/sendErrorResponse.ts
+++ app/utils/functions/api/sendErrorResponse.ts
@@ -1,5 +1,5 @@
 import {ApiError} from '@appTypes/api';
-import {HTTP_SERVER_ERROR, MSG_DUPLICATE_ERROR, MSG_SERVER_ERROR} from '@constants/api';
+import {HTTP_SCHEMA_ERROR, HTTP_SERVER_ERROR, MSG_DUPLICATE_ERROR, MSG_SERVER_ERROR} from '@constants/api';
 
 /**
  * Construct and send an error response
@@ -17,13 +17,19 @@ const sendErrorResponse = (error: any): Response => {
     },
   };
 
-  // si l'erreur concerne un enregistrement déjà existant
+  // if the error is a duplicate error
   if (error.code === 'P2002') {
     apiError.error.message = MSG_DUPLICATE_ERROR;
     apiError.error.details = {
       field: error.meta!.target[0],
-      message: `A record for the ${error.meta!.modelName} table with the ${error.meta!.target} value already exists`,
+      message: `A record for the ${error.meta!.modelName} table with this ${error.meta!.target} value already exists`,
     };
+  }
+
+  // if the error is a schema validation error
+  if (error.status === HTTP_SCHEMA_ERROR) {
+    apiError.error.code = HTTP_SCHEMA_ERROR;
+    apiError.error.details = error.messages;
   }
 
   return Response.json(apiError, {status: HTTP_SERVER_ERROR});
