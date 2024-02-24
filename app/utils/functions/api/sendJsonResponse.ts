62b9008dbd13a4fff8eea122b8200279dc02ecf0
Timeuh <timeuuh@gmail.com>
2024-02-24 12:01:51 +0100

    :sparkles: create json response send function

---
+++ app/utils/functions/api/sendJsonResponse.ts
@@ -0,0 +1,9 @@
+/**
+ * Send a response in json format
+ *
+ * @param data the data object to send
+ * @param code the http status code of the response
+ */
+const sendJsonResponse = (data: Object, code: number) => {
+  return Response.json(data, {status: code});
+};
