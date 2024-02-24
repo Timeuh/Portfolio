37aa271ba045e4d47dc9bccd39bbec3ed7e58c4f
Timeuh <timeuuh@gmail.com>
2024-02-24 11:49:26 +0100

    :sparkles: add http response codes constants

---
+++ app/utils/constants/api.ts
@@ -0,0 +1,18 @@
+/* -------------------------------------------------------------------------- */
+/*                            HTTP Response codes                             */
+/* -------------------------------------------------------------------------- */
+
+// request successful
+export const HTTP_OK = 200;
+
+// resource created
+export const HTTP_CREATED = 201;
+
+// resource not found
+export const HTTP_NOT_FOUND = 404;
+
+// schema validation failed
+export const HTTP_SCHEMA_ERROR = 422;
+
+// any server error
+export const HTTP_SERVER_ERROR = 500;
