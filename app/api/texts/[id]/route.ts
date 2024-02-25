b27cf9b4b5ef53c65c6864aad0373f126ec32775
Timeuh <timeuuh@gmail.com>
2024-02-25 12:06:07 +0100

    :sparkles: create text update route handler

--- app/api/texts/[id]/route.ts
+++ app/api/texts/[id]/route.ts
@@ -3,7 +3,7 @@ import {prisma} from '@utils/prisma/client';
 import {HTTP_NOT_FOUND, HTTP_OK, MSG_NOT_FOUND} from '@constants/api';
 import sendJsonResponse from '@functions/api/sendJsonResponse';
 import {ApiError, ApiParams} from '@appTypes/api';
-import {TextFromApi, TextFromDatabase} from '@schemas/api/text/text.schema';
+import {Text, TextFromApi, TextFromDatabase, textValidator} from '@schemas/api/text/text.schema';
 import formatTextForApi from '@functions/api/text/formatTextForApi';
 
 /**
@@ -36,6 +36,38 @@ export async function GET(request: Request, apiParams: ApiParams): Promise<Respo
     }
 
     const textToReturn: TextFromApi = formatTextForApi(text) as TextFromApi;
+
+    return sendJsonResponse<TextFromApi>(textToReturn, HTTP_OK);
+  } catch (error: any) {
+    return sendErrorResponse(error);
+  }
+}
+
+/**
+ * Update a text by id
+ *
+ * @param {Request} request the request data object
+ * @param {ApiParams} apiParams the request parameters
+ *
+ * @returns {Promise<Response>} a promise containing the updated text in json format
+ */
+export async function PUT(request: Request, apiParams: ApiParams): Promise<Response> {
+  try {
+    const body = await request.json();
+
+    const parsedBody: Text = await textValidator.validate(body);
+
+    const updatedText: TextFromDatabase = await prisma.text.update({
+      where: {
+        id: parseInt(apiParams.params.id),
+      },
+      data: {
+        english: parsedBody.english,
+        french: parsedBody.french,
+      },
+    });
+
+    const textToReturn: TextFromApi = formatTextForApi(updatedText) as TextFromApi;
 
     return sendJsonResponse<TextFromApi>(textToReturn, HTTP_OK);
   } catch (error: any) {
