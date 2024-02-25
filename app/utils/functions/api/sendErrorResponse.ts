e46199c73660785d0e02edb366129b7c45ea3ae5
Timeuh <timeuuh@gmail.com>
2024-02-25 10:28:39 +0100

    :sparkles: handle prisma record to update not found error

--- app/utils/functions/api/sendErrorResponse.ts
+++ app/utils/functions/api/sendErrorResponse.ts
@@ -1,5 +1,12 @@
 import {ApiError} from '@appTypes/api';
-import {HTTP_SCHEMA_ERROR, HTTP_SERVER_ERROR, MSG_DUPLICATE_ERROR, MSG_SERVER_ERROR} from '@constants/api';
+import {
+  HTTP_NOT_FOUND,
+  HTTP_SCHEMA_ERROR,
+  HTTP_SERVER_ERROR,
+  MSG_DUPLICATE_ERROR,
+  MSG_NOT_FOUND,
+  MSG_SERVER_ERROR,
+} from '@constants/api';
 
 /**
  * Construct and send an error response
@@ -26,13 +33,20 @@ const sendErrorResponse = (error: any): Response => {
     };
   }
 
+  // if the record to update does not exist
+  if (error.code === 'P2025') {
+    apiError.error.code = HTTP_NOT_FOUND;
+    apiError.error.message = MSG_NOT_FOUND;
+    apiError.error.details = `Can not update table ${error.meta!.modelName} because the record does not exist`;
+  }
+
   // if the error is a schema validation error
   if (error.status === HTTP_SCHEMA_ERROR) {
     apiError.error.code = HTTP_SCHEMA_ERROR;
     apiError.error.details = error.messages;
   }
 
-  return Response.json(apiError, {status: HTTP_SERVER_ERROR});
+  return Response.json(apiError, {status: apiError.error.code});
 };
 
 export default sendErrorResponse;
