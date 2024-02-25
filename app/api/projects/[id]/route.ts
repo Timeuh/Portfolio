dcc9e6bd6347047784787af7a3e6c7889642fa76
Timeuh <timeuuh@gmail.com>
2024-02-25 17:24:13 +0100

    :sparkles: create project update route handler

--- app/api/projects/[id]/route.ts
+++ app/api/projects/[id]/route.ts
@@ -3,7 +3,12 @@ import {prisma} from '@utils/prisma/client';
 import {HTTP_NOT_FOUND, HTTP_OK, MSG_NOT_FOUND} from '@constants/api';
 import sendJsonResponse from '@functions/api/sendJsonResponse';
 import {ApiError, ApiParams} from '@appTypes/api';
-import {ProjectFromApi, ProjectFromDatabase} from '@schemas/api/project/project.schema';
+import {
+  ProjectFromApi,
+  ProjectFromDatabase,
+  ProjectUpsert,
+  projectUpsertValidator,
+} from '@schemas/api/project/project.schema';
 import formatProjectForApi from '@functions/api/project/formatProjectForApi';
 
 /**
@@ -36,6 +41,46 @@ export async function GET(request: Request, apiParams: ApiParams): Promise<Respo
     }
 
     const projectToReturn: ProjectFromApi = formatProjectForApi(project) as ProjectFromApi;
+
+    return sendJsonResponse<ProjectFromApi>(projectToReturn, HTTP_OK);
+  } catch (error: any) {
+    return sendErrorResponse(error);
+  }
+}
+
+/**
+ * Update a project by id
+ *
+ * @param {Request} request the request data object
+ * @param {ApiParams} apiParams the request parameters
+ *
+ * @returns {Promise<Response>} a promise containing the updated project in json format
+ */
+export async function PUT(request: Request, apiParams: ApiParams): Promise<Response> {
+  try {
+    const body = await request.json();
+
+    const parsedBody: ProjectUpsert = await projectUpsertValidator.validate(body);
+
+    const updatedProject: ProjectFromDatabase = await prisma.project.update({
+      where: {
+        id: parseInt(apiParams.params.id),
+      },
+      data: {
+        description: {
+          update: {
+            english: parsedBody.description.english,
+            french: parsedBody.description.french,
+          },
+        },
+        gif: parsedBody.gif,
+        github: parsedBody.github,
+        live_version: parsedBody.live_version,
+        name: parsedBody.name,
+      },
+    });
+
+    const projectToReturn: ProjectFromApi = formatProjectForApi(updatedProject) as ProjectFromApi;
 
     return sendJsonResponse<ProjectFromApi>(projectToReturn, HTTP_OK);
   } catch (error: any) {
