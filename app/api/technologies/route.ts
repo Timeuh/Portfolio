7447be52a8d63ff2a249d667a81076b3f95def9f
Timeuh <timeuuh@gmail.com>
2024-02-25 16:24:52 +0100

    :sparkles: create technology create route handler

--- app/api/technologies/route.ts
+++ app/api/technologies/route.ts
@@ -1,8 +1,15 @@
 import sendErrorResponse from '@functions/api/sendErrorResponse';
 import {prisma} from '@utils/prisma/client';
 import sendCollectionResponse from '@functions/api/sendCollectionResponse';
-import {TechnologyFromApi, TechnologyFromDatabase} from '@schemas/api/technology/technology.schema';
+import {
+  TechnologyFromApi,
+  TechnologyFromDatabase,
+  TechnologyUpsert,
+  technologyUpsertValidator,
+} from '@schemas/api/technology/technology.schema';
 import formatTechnologyForApi from '@functions/api/technology/formatTechnologyForApi';
+import {HTTP_CREATED} from '@constants/api';
+import sendJsonResponse from '@functions/api/sendJsonResponse';
 
 /**
  * Get all technologies
@@ -20,6 +27,46 @@ export async function GET(request: Request): Promise<Response> {
     });
 
     return sendCollectionResponse<TechnologyFromApi>(technologiesToReturn);
+  } catch (error: any) {
+    return sendErrorResponse(error);
+  }
+}
+
+/**
+ * Create a new technology
+ *
+ * @param {Request} request the request data object
+ *
+ * @returns {Promise<Response>} a promise containing the created technology in json format
+ */
+export async function POST(request: Request): Promise<Response> {
+  try {
+    const body = await request.json();
+
+    const parsedBody: TechnologyUpsert = await technologyUpsertValidator.validate(body);
+
+    const createdTechnology: TechnologyFromDatabase = await prisma.technology.create({
+      data: {
+        category: {
+          connect: {
+            id: parsedBody.category_id,
+          },
+        },
+        color: parsedBody.color,
+        description: {
+          create: {
+            english: parsedBody.description.english,
+            french: parsedBody.description.french,
+          },
+        },
+        logo: parsedBody.logo,
+        name: parsedBody.name,
+      },
+    });
+
+    const technologyToReturn: TechnologyFromApi = formatTechnologyForApi(createdTechnology) as TechnologyFromApi;
+
+    return sendJsonResponse<TechnologyFromApi>(technologyToReturn, HTTP_CREATED);
   } catch (error: any) {
     return sendErrorResponse(error);
   }
