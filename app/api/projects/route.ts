1f1414e89b03d81ee28f05e81fc00d3b054e3a3d
Timeuh <timeuuh@gmail.com>
2024-02-25 17:13:24 +0100

    :sparkles: create project create handler

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -1,8 +1,15 @@
 import sendErrorResponse from '@functions/api/sendErrorResponse';
 import {prisma} from '@utils/prisma/client';
 import sendCollectionResponse from '@functions/api/sendCollectionResponse';
-import {ProjectFromApi, ProjectFromDatabase} from '@schemas/api/project/project.schema';
+import {
+  ProjectFromApi,
+  ProjectFromDatabase,
+  ProjectUpsert,
+  projectUpsertValidator,
+} from '@schemas/api/project/project.schema';
 import formatProjectForApi from '@functions/api/project/formatProjectForApi';
+import {HTTP_CREATED} from '@constants/api';
+import sendJsonResponse from '@functions/api/sendJsonResponse';
 
 /**
  * Get all technologies
@@ -20,6 +27,42 @@ export async function GET(request: Request): Promise<Response> {
     });
 
     return sendCollectionResponse<ProjectFromApi>(technologiesToReturn);
+  } catch (error: any) {
+    return sendErrorResponse(error);
+  }
+}
+
+/**
+ * Create a new project
+ *
+ * @param {Request} request the request data object
+ *
+ * @returns {Promise<Response>} a promise containing the created project in json format
+ */
+export async function POST(request: Request): Promise<Response> {
+  try {
+    const body = await request.json();
+
+    const parsedBody: ProjectUpsert = await projectUpsertValidator.validate(body);
+
+    const createdProject: ProjectFromDatabase = await prisma.project.create({
+      data: {
+        description: {
+          create: {
+            english: parsedBody.description.english,
+            french: parsedBody.description.french,
+          },
+        },
+        gif: parsedBody.gif,
+        github: parsedBody.github,
+        live_version: parsedBody.live_version,
+        name: parsedBody.name,
+      },
+    });
+
+    const projectToReturn: ProjectFromApi = formatProjectForApi(createdProject) as ProjectFromApi;
+
+    return sendJsonResponse<ProjectFromApi>(projectToReturn, HTTP_CREATED);
   } catch (error: any) {
     return sendErrorResponse(error);
   }
