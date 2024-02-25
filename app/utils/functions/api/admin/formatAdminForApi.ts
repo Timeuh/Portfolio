e42797af89f5ee1572e37f7d6ffd3abd6ba5d8a9
Timeuh <timeuuh@gmail.com>
2024-02-25 10:55:11 +0100

    :sparkles: add admin when deleted format to admin formatting function

--- app/utils/functions/api/admin/formatAdminForApi.ts
+++ app/utils/functions/api/admin/formatAdminForApi.ts
@@ -1,13 +1,21 @@
-import {Admin, AdminFromDatabase} from '@schemas/api/admin/admin.schema';
+import {Admin, AdminFromDatabase, AdminWhenDeleted} from '@schemas/api/admin/admin.schema';
 
 /**
  * Format an admin for the api return
  *
  * @param {AdminFromDatabase} admin the admin to format
+ * @param {boolean} isDeleted if the admin is deleted
  *
- * @returns {Admin} the formatted admin
+ * @returns {Admin | AdminWhenDeleted} the formatted admin
  */
-const formatAdminForApi = (admin: AdminFromDatabase): Admin => {
+const formatAdminForApi = (admin: AdminFromDatabase, isDeleted: boolean = false): Admin | AdminWhenDeleted => {
+  if (isDeleted) {
+    return {
+      email: admin.email,
+      id: admin.id,
+    } as AdminWhenDeleted;
+  }
+
   return {
     email: admin.email,
     id: admin.id,
@@ -25,7 +33,7 @@ const formatAdminForApi = (admin: AdminFromDatabase): Admin => {
         href: `/api/admins/${admin.id}`,
       },
     },
-  };
+  } as Admin;
 };
 
 export default formatAdminForApi;
