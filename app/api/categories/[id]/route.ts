81eda064d0b3d9fbd0b74bfac197cdf9fb27f377
Timeuh <timeuuh@gmail.com>
2024-02-25 15:18:28 +0100

    :sparkles: create category update route handler

--- app/api/categories/[id]/route.ts
+++ app/api/categories/[id]/route.ts
@@ -3,7 +3,12 @@ import {prisma} from '@utils/prisma/client';
 import {HTTP_NOT_FOUND, HTTP_OK, MSG_NOT_FOUND} from '@constants/api';
 import sendJsonResponse from '@functions/api/sendJsonResponse';
 import {ApiError, ApiParams} from '@appTypes/api';
-import {CategoryFromApi, CategoryFromDatabase} from '@schemas/api/category/category.schema';
+import {
+  CategoryFromApi,
+  CategoryFromDatabase,
+  CategoryUpsert,
+  categoryUpsertValidator,
+} from '@schemas/api/category/category.schema';
 import formatCategoryForApi from '@functions/api/category/formatCategoryForApi';
 
 /**
@@ -36,6 +41,43 @@ export async function GET(request: Request, apiParams: ApiParams): Promise<Respo
     }
 
     const categoryToReturn: CategoryFromApi = formatCategoryForApi(category) as CategoryFromApi;
+
+    return sendJsonResponse<CategoryFromApi>(categoryToReturn, HTTP_OK);
+  } catch (error: any) {
+    return sendErrorResponse(error);
+  }
+}
+
+/**
+ * Update a category by id
+ *
+ * @param {Request} request the request data object
+ * @param {ApiParams} apiParams the request parameters
+ *
+ * @returns {Promise<Response>} a promise containing the updated category in json format
+ */
+export async function PUT(request: Request, apiParams: ApiParams): Promise<Response> {
+  try {
+    const body = await request.json();
+
+    const parsedBody: CategoryUpsert = await categoryUpsertValidator.validate(body);
+
+    const updatedCategory: CategoryFromDatabase = await prisma.category.update({
+      where: {
+        id: parseInt(apiParams.params.id),
+      },
+      data: {
+        logo: parsedBody.logo,
+        name: {
+          update: {
+            english: parsedBody.name.english,
+            french: parsedBody.name.french,
+          },
+        },
+      },
+    });
+
+    const categoryToReturn: CategoryFromApi = formatCategoryForApi(updatedCategory) as CategoryFromApi;
 
     return sendJsonResponse<CategoryFromApi>(categoryToReturn, HTTP_OK);
   } catch (error: any) {
