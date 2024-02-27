144d0157d6e6d49e46e9f944fc83883cb6d76f74
Timeuh <timeuuh@gmail.com>
2024-02-27 16:47:21 +0100

    :safety_vest: add schema and type for complete category returned by database

--- app/vine/api/category/category.schema.ts
+++ app/vine/api/category/category.schema.ts
@@ -2,7 +2,6 @@ import vine from '@vinejs/vine';
 import {Infer} from '@vinejs/vine/types';
 import {linksForCategorySchema} from '@schemas/api/links/links.schema';
 import {textForOthersSchema, textUpsertSchema} from '@schemas/api/text/text.schema';
-import {technologyForOthersSchema} from '@schemas/api/technology/technology.schema';
 
 /* -------------------------------------------------------------------------- */
 /*                                  Schemas                                   */
@@ -25,7 +24,15 @@ const categoryFromApiSchema = vine.object({
 const completeCategoryFromApiSchema = vine.object({
   ...baseCategorySchema.getProperties(),
   name: textForOthersSchema.clone(),
-  technologies: vine.array(technologyForOthersSchema.clone()),
+  technologies: vine.array(
+    vine.object({
+      id: vine.number(),
+      name: vine.string(),
+      logo: vine.string(),
+      color: vine.string(),
+      description: textForOthersSchema.clone(),
+    }),
+  ),
   links: linksForCategorySchema.clone(),
 });
 
@@ -47,6 +54,22 @@ const categoryFromDatabaseSchema = vine.object({
   name_id: vine.number(),
 });
 
+// complete category returned by database
+const completeCategoryFromDatabaseSchema = vine.object({
+  ...baseCategorySchema.getProperties(),
+  name_id: vine.number(),
+  name: textForOthersSchema.clone(),
+  technologies: vine.array(
+    vine.object({
+      id: vine.number(),
+      name: vine.string(),
+      logo: vine.string(),
+      color: vine.string(),
+      description: textForOthersSchema.clone(),
+    }),
+  ),
+});
+
 /* -------------------------------------------------------------------------- */
 /*                                   Types                                    */
 /* -------------------------------------------------------------------------- */
@@ -65,6 +88,9 @@ export type CategoryWhenDeleted = Infer<typeof categoryWhenDeletedSchema>;
 
 // type for category returned by database
 export type CategoryFromDatabase = Infer<typeof categoryFromDatabaseSchema>;
+
+// type for complete category returned by database
+export type CompleteCategoryFromDatabase = Infer<typeof completeCategoryFromDatabaseSchema>;
 
 /* -------------------------------------------------------------------------- */
 /*                                 Validators                                 */
