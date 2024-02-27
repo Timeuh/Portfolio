f74d3f9c27899a490b870927108a3f08b12bed41
Timeuh <timeuuh@gmail.com>
2024-02-27 17:26:31 +0100

    :sparkles: format complete project when provided

--- app/utils/functions/api/project/formatProjectForApi.ts
+++ app/utils/functions/api/project/formatProjectForApi.ts
@@ -1,17 +1,25 @@
-import {ProjectFromApi, ProjectFromDatabase, ProjectWhenDeleted} from '@schemas/api/project/project.schema';
+import {
+  CompleteProjectFromApi,
+  CompleteProjectFromDatabase,
+  ProjectFromApi,
+  ProjectFromDatabase,
+  ProjectWhenDeleted,
+} from '@schemas/api/project/project.schema';
 
 /**
  * Format a project for the api return
  *
- * @param {ProjectFromDatabase} project the project to format
+ * @param {ProjectFromDatabase | CompleteProjectFromDatabase} project the project to format
  * @param {boolean} isDeleted if the project is deleted
+ * @param {boolean} isFull if the project is complete
  *
- * @returns {ProjectFromApi | ProjectWhenDeleted} the formatted project
+ * @returns {ProjectFromApi | ProjectWhenDeleted | CompleteProjectFromApi} the formatted project
  */
 const formatProjectForApi = (
-  project: ProjectFromDatabase,
+  project: ProjectFromDatabase | CompleteProjectFromDatabase,
   isDeleted: boolean = false,
-): ProjectFromApi | ProjectWhenDeleted => {
+  isFull: boolean = false,
+): ProjectFromApi | ProjectWhenDeleted | CompleteProjectFromApi => {
   if (isDeleted) {
     return {
       description_id: project.description_id,
@@ -21,6 +29,53 @@ const formatProjectForApi = (
       live_version: project.live_version,
       name: project.name,
     } as ProjectWhenDeleted;
+  }
+
+  if (isFull && 'Project_Technologies' in project) {
+    return {
+      description: {
+        english: project.description.english,
+        french: project.description.french,
+        id: project.description.id,
+      },
+      gif: project.gif,
+      github: project.github,
+      id: project.id,
+      live_version: project.live_version,
+      name: project.name,
+      technologies: project.Project_Technologies.map((association) => {
+        return {
+          category_id: association.technology.category_id,
+          color: association.technology.color,
+          description_id: association.technology.description_id,
+          id: association.technology.id,
+          logo: association.technology.logo,
+          name: association.technology.name,
+        };
+      }),
+      links: {
+        self: {
+          method: 'GET',
+          href: `/api/projects/${project.id}`,
+        },
+        update: {
+          method: 'PUT',
+          href: `/api/projects/${project.id}`,
+        },
+        delete: {
+          method: 'DELETE',
+          href: `/api/projects/${project.id}`,
+        },
+        gif: {
+          method: 'GET',
+          href: `/api/logos/gifs/${project.gif}`,
+        },
+        description: {
+          method: 'GET',
+          href: `/api/texts/${project.description_id}`,
+        },
+      },
+    } as CompleteProjectFromApi;
   }
 
   return {
