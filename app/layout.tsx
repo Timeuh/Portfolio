a88459d3203752e4d178184437c41a551818fda4
Timeuh <timeuuh@gmail.com>
2024-02-28 21:17:26 +0100

    :sparkles: use navbar in layout + use rubik font for all pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type {Metadata} from 'next';
 import './globals.css';
 import {PropsWithChildren} from 'react';
 import {BricolageGroteske, Rubik} from '@assets/fonts/fonts';
+import Navbar from '@components/Navbar';
+import {css} from '@/styled-system/css';
 
 export const metadata: Metadata = {
   title: 'Timeuh - Portfolio',
@@ -11,7 +13,10 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: PropsWithChildren) {
   return (
     <html lang='fr' className={`${Rubik.variable} ${BricolageGroteske.variable}`}>
-      <body>{children}</body>
+      <body className={css({fontFamily: 'rubik'})}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
