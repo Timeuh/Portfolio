c8ac674bf08e9f46b98a7a7cd9002b04566fc455
Timeuh <timeuuh@gmail.com>
2024-02-29 19:21:33 +0100

    :label: add langage context related types

--- app/utils/types/portfolio.ts
+++ app/utils/types/portfolio.ts
@@ -1,2 +1,11 @@
 // app theme
 export type Theme = 'light' | 'dark';
+
+// app langage
+export type Langage = 'fr' | 'en';
+
+// langage context exported utilities
+export type LangageContextUtils = {
+  langage: Langage;
+  setLangage: (langage: Langage) => void;
+};
