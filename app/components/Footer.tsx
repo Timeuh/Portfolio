19c59af2eeef5df6d37ab5610abc680d2722cc04
Timeuh <timeuuh@gmail.com>
2024-03-03 11:48:21 +0100

    :lipstick: adapt footer to tablet and desktop

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -11,49 +11,79 @@ export default function Footer() {
   const {langage} = useLangage();
 
   return (
-    <footer>
-      <div
-        className={grid({
-          marginTop: 40,
-          columns: 3,
-          gap: 2,
-          padding: 2,
-          fontSize: '10px',
-          fontWeight: 'bold',
-          fontFamily: 'bricolage',
-          shadow: {
-            base: '0px -4px 4px 0 rgba(0, 26, 56, 0.25)',
-            _dark: '0px -4px 4px 0 rgba(240, 247, 255, 0.25)',
-          },
-          color: {
-            base: 'neutral.dark',
-            _dark: 'neutral.light',
-          },
-          bgColor: {
-            base: 'p_blue.light',
-            _dark: 'p_blue.dark',
-          },
-        })}
-      >
-        {footerLinks.map((link: FooterLink, index: number) => {
-          return (
-            <Link key={index} href={link.href}>
-              {link.title[langage]}
-              <span
-                className={css({
-                  display: !link.span ? 'none' : 'inline',
-                  color: {
-                    base: 'p_purple.main',
-                    _dark: 'p_blue.main',
-                  },
-                })}
-              >
-                {!link.span ? '' : link.span.text}
-              </span>
-            </Link>
-          );
-        })}
-      </div>
+    <footer
+      className={grid({
+        justifyItems: {
+          xl: 'center',
+        },
+        columns: {
+          base: 3,
+          xl: 6,
+        },
+        gap: {
+          base: 2,
+          md: 4,
+          xl: 0,
+        },
+        rowGap: 4,
+        padding: {
+          base: 2,
+          md: 4,
+        },
+        paddingX: {
+          md: 12,
+          xl: 24,
+        },
+        fontSize: {
+          base: '10px',
+          md: 'md',
+        },
+        textAlign: {
+          xl: 'center',
+        },
+        fontWeight: 'bold',
+        fontFamily: 'bricolage',
+        shadow: {
+          base: '0px -4px 4px 0 rgba(0, 26, 56, 0.25)',
+          _dark: '0px -4px 4px 0 rgba(240, 247, 255, 0.25)',
+        },
+        color: {
+          base: 'neutral.dark',
+          _dark: 'neutral.light',
+        },
+        bgColor: {
+          base: 'p_blue.light',
+          _dark: 'p_blue.dark',
+        },
+      })}
+    >
+      {footerLinks.map((link: FooterLink, index: number) => {
+        return (
+          <Link
+            key={index}
+            href={link.href}
+            className={css({
+              width: 'fit',
+              textDecoration: {
+                _hover: 'underline',
+              },
+            })}
+          >
+            {link.title[langage]}
+            <span
+              className={css({
+                display: !link.span ? 'none' : 'inline',
+                color: {
+                  base: 'p_purple.main',
+                  _dark: 'p_blue.main',
+                },
+              })}
+            >
+              {!link.span ? '' : link.span.text}
+            </span>
+          </Link>
+        );
+      })}
     </footer>
   );
 }
