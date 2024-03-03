977820db6611cdae5ff43ada78d70d0ca731d6e3
Timeuh <timeuuh@gmail.com>
2024-03-03 18:06:51 +0100

    :speech_balloon: add landing section role title text

---
+++ app/texts/portfolio/landing/roleTitle.ts
@@ -0,0 +1,8 @@
+import {AppText} from '@appTypes/portfolio';
+
+const roleTitle: AppText = {
+  en: 'Student and future Fullstack React developer',
+  fr: 'Étudiant et futur développeur Fullstack React',
+};
+
+export default roleTitle;
