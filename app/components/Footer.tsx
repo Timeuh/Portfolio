74feffa39574f1cee9d3c9112175bab152a8d877
Timeuh <timeuuh@gmail.com>
2024-03-05 14:02:44 +0100

    :recycle: move footer styles in dedicated styles file

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,86 +1,21 @@
 'use client';
 
-import {grid} from '@/styled-system/patterns';
 import footerLinks from '@app/texts/portfolio/footer/footerLinks';
 import Link from 'next/link';
 import {FooterLink} from '@appTypes/portfolio';
 import useLangage from '@hooks/useLangage';
-import {css} from '@/styled-system/css';
+import {footerStyle, getSpanStyle, linkStyle} from '@styles/components/Footer.styles';
 
 export default function Footer() {
   const {langage} = useLangage();
 
   return (
-    <footer
-      className={grid({
-        justifyItems: {
-          xl: 'center',
-        },
-        columns: {
-          base: 3,
-          xl: 6,
-        },
-        gap: {
-          base: 2,
-          md: 4,
-          xl: 0,
-        },
-        rowGap: 4,
-        padding: {
-          base: 2,
-          md: 4,
-        },
-        paddingX: {
-          md: 12,
-          xl: 24,
-        },
-        fontSize: {
-          base: '10px',
-          md: 'md',
-        },
-        textAlign: {
-          xl: 'center',
-        },
-        fontWeight: 'bold',
-        fontFamily: 'bricolage',
-        shadow: {
-          base: '0px -4px 4px 0 token(colors.p_blue.dark_25)',
-          _dark: '0px -4px 4px 0 token(colors.p_blue.light_25)',
-        },
-        color: {
-          base: 'neutral.dark',
-          _dark: 'neutral.light',
-        },
-        bgColor: {
-          base: 'p_blue.light',
-          _dark: 'p_blue.dark',
-        },
-      })}
-    >
+    <footer className={footerStyle}>
       {footerLinks.map((link: FooterLink, index: number) => {
         return (
-          <Link
-            key={index}
-            href={link.href}
-            className={css({
-              width: 'fit',
-              textDecoration: {
-                _hover: 'underline',
-              },
-            })}
-          >
+          <Link key={index} href={link.href} className={linkStyle}>
             {link.title[langage]}
-            <span
-              className={css({
-                display: !link.span ? 'none' : 'inline',
-                color: {
-                  base: 'p_purple.main',
-                  _dark: 'p_blue.main',
-                },
-              })}
-            >
-              {!link.span ? '' : link.span.text}
-            </span>
+            <span className={getSpanStyle(link)}>{!link.span ? '' : link.span.text}</span>
           </Link>
         );
       })}
