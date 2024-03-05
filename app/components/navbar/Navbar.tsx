130f99d2a94494057710f096d4699cf817a129c9
Timeuh <timeuuh@gmail.com>
2024-03-05 14:45:33 +0100

    :recycle: move navbar styles in dedicated styles file

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,66 +1,16 @@
-import {css} from '@/styled-system/css';
-import {hstack} from '@/styled-system/patterns';
 import MenuButton from '@components/navbar/buttons/MenuButton';
 import ThemeButton from '@components/navbar/buttons/ThemeButton';
 import LangageButton from '@components/navbar/buttons/LangageButton';
 import NavLinks from '@components/navbar/NavLinks';
+import {divStyle, nameStyle, navStyle} from '@styles/components/navbar/Navbar.styles';
 
 export default function Navbar() {
   return (
-    <nav
-      className={hstack({
-        zIndex: 10,
-        bgColor: {
-          base: 'p_purple.light',
-          _dark: 'p_purple.dark',
-        },
-        height: {
-          base: 12,
-          md: 20,
-          xl: 14,
-        },
-        width: 'full',
-        shadow: {
-          base: '0 4px 4px 0 token(colors.p_purple.dark_25)',
-          _dark: '0 4px 4px 0 token(colors.p_purple.light_25)',
-        },
-        justifyContent: {
-          base: 'space-around',
-          xl: 'space-between',
-        },
-        paddingX: {
-          base: 2,
-          xl: 4,
-        },
-        position: 'fixed',
-        transition: 'background-color 0.5s',
-      })}
-    >
+    <nav className={navStyle}>
       <MenuButton />
-      <h2
-        className={css({
-          fontSize: {
-            base: 'lg',
-            md: '4xl',
-            xl: '2xl',
-          },
-          fontWeight: 'bold',
-          background: {
-            base: 'linear-gradient(to right, token(colors.p_blue.dark), token(colors.p_blue.main), token(colors.p_purple.main), token(colors.p_purple.dark))',
-            _dark:
-              'linear-gradient(to right, token(colors.p_blue.light), token(colors.p_blue.main), token(colors.p_purple.main), token(colors.p_purple.light))',
-          },
-          color: 'transparent',
-          bgClip: {
-            base: 'text',
-            _dark: 'text',
-          },
-        })}
-      >
-        Timothée Brindejonc
-      </h2>
+      <h2 className={nameStyle}>Timothée Brindejonc</h2>
       <NavLinks />
-      <div className={hstack({gap: 4})}>
+      <div className={divStyle}>
         <ThemeButton />
         <LangageButton />
       </div>
