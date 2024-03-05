19d5cb2f2201877f599f4a2a8c596273454f31d8
Timeuh <timeuuh@gmail.com>
2024-03-05 14:39:38 +0100

    :recycle: move navlinks styles in dedicated styles file

--- app/components/navbar/NavLinks.tsx
+++ app/components/navbar/NavLinks.tsx
@@ -2,78 +2,27 @@
 
 import appLinks from '@app/texts/portfolio/navbar/appLinks';
 import Link from 'next/link';
-import {css} from '@/styled-system/css';
 import useLangage from '@hooks/useLangage';
 import {useState} from 'react';
-import {hstack} from '@/styled-system/patterns';
+import {divStyle, getLinkStyle, getSelectedLinkStyle} from '@styles/components/navbar/NavLinks.styles';
 
 export default function NavLinks() {
   const {langage} = useLangage();
   const [active, setActive] = useState<number>(0);
 
   return (
-    <div
-      className={hstack({
-        gap: 20,
-        textAlign: 'center',
-        display: {
-          base: 'none',
-          xl: 'flex',
-        },
-      })}
-    >
+    <div className={divStyle}>
       {appLinks.map((link, index) => {
         return (
           <Link
             key={index}
             href={link.href}
-            className={css({
-              width: 'full',
-              paddingX: 2,
-              paddingY: 3,
-              fontSize: '2xl',
-              fontWeight: 'bold',
-              position: 'relative',
-              color: {
-                base: active === index ? 'p_purple.800' : 'neutral.700',
-                _hover: active === index ? 'p_purple.700' : 'p_blue.700',
-                _dark: {
-                  base: active === index ? 'p_purple.400' : 'neutral.light',
-                  _hover: active === index ? 'p_purple.200' : 'p_blue.300',
-                },
-              },
-              transition: 'color 0.5s',
-            })}
+            className={getLinkStyle(active, index)}
             onClick={() => {
               setActive(index);
             }}
           >
-            <div
-              className={css({
-                position: 'absolute',
-                width: 'full',
-                height: 2,
-                left: 0,
-                bottom: 0,
-                bgGradient: 'to-t',
-                gradientFrom: {
-                  base: 'p_purple.700',
-                  _dark: 'p_purple.light',
-                },
-                gradientTo: {
-                  base: 'p_purple.light',
-                  _dark: 'p_purple.dark',
-                },
-                borderTopRadius: 'xl',
-                shadow: '2px 0 4px 0',
-                shadowColor: {
-                  base: 'p_purple.dark/25',
-                  _dark: 'p_purple.light/25',
-                },
-                display: active === index ? 'block' : 'none',
-                transition: 'all 0.5s',
-              })}
-            ></div>
+            <div className={getSelectedLinkStyle(active, index)}></div>
             {link.title[langage]}
           </Link>
         );
