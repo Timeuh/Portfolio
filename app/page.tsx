6f90f1863fbce7f9eb1f7a90c5d3f637c9bdb1c9
Timeuh <timeuuh@gmail.com>
2024-03-06 22:43:20 +0100

    :sparkles: use experience view in page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import LandingView from '@views/LandingView';
 import JourneyView from '@views/JourneyView';
+import ExperienceView from '@views/ExperienceView';
 
 export default function Home() {
   return (
     <main>
       <LandingView />
       <JourneyView />
+      <ExperienceView />
     </main>
   );
 }
