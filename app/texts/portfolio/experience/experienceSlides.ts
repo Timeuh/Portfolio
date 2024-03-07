b5e43066989931e00bbb6e05262fc059eeae0f05
Timeuh <timeuuh@gmail.com>
2024-03-07 12:26:27 +0100

    :speech_balloon: add texts for experience slides

---
+++ app/texts/portfolio/experience/experienceSlides.ts
@@ -0,0 +1,36 @@
+import {Slide} from '@components/carousel/Carousel';
+
+const experienceSlides: Slide[] = [
+  {
+    title: {
+      en: 'Knowing more',
+      fr: 'Pour en savoir plus',
+    },
+    content: {
+      en: 'Of course, all my experiences are not summed up in so few words. If you want to know more about what I had the opportunity to do during these experiences, you can discover it below.',
+      fr: 'Bien sûr, toutes mes expériences ne se résument pas en si peu de mots. Si vous souhaitez en savoir plus sur ce que j’ai eu l’occasion de faire durant ces expériences, vous pouvez le découvrir ci-dessous.',
+    },
+  },
+  {
+    title: {
+      en: 'During my formation',
+      fr: 'Durant ma formation',
+    },
+    content: {
+      en: 'To validate my Bachelor’s degree in computer science, I had to complete a 10-week internship in my 2nd year. I then chose to spend my 3rd year in alternating (in progress). Although being alone on my alternate project, I am hopeful of learning a lot.',
+      fr: 'Pour valider ma formation de BUT informatique, j’ai dû effectuer un stage de 10 semaines en 2ème année. J’ai ensuite choisi de passer ma 3ème année en alternance (en cours). Bien que tout seul sur mon projet d’alternance, j’ai bon espoir d’en apprendre beaucoup.',
+    },
+  },
+  {
+    title: {
+      en: 'On my initiative',
+      fr: 'De mon initiative',
+    },
+    content: {
+      en: 'During my 2nd year internship, I got closer to my colleagues and was charmed by the atmosphere in the team. I took the opportunity to negotiate a 1-month fixed-term contract in August 2023 during which I discovered new programming concepts.',
+      fr: 'Lors de mon stage de 2ème année, je me suis rapproché de mes collègues et j’ai été charmé par l’ambiance dans l’équipe. J’en ai profité pour négocier un CDD de 1 mois en Août 2023 durant lequel j’ai découvert de nouvelles notions de programmation.',
+    },
+  },
+];
+
+export default experienceSlides;
