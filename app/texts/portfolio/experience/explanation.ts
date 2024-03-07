d5acbd815f624220406cd579749612b36c8a8698
Timeuh <timeuuh@gmail.com>
2024-03-07 14:17:21 +0100

    :speech_balloon: create experience explanation text

---
+++ app/texts/portfolio/experience/explanation.ts
@@ -0,0 +1,33 @@
+import {ExperienceExplanation} from '@appTypes/portfolio';
+
+// experience section explanations
+const explanation: ExperienceExplanation = {
+  title: {
+    en: 'My experiences',
+    fr: 'Mes expériences',
+  },
+  content: {
+    firstPart: {
+      en: 'During my ',
+      fr: 'Au cours de ma ',
+    },
+    firstSpan: {
+      en: 'formation',
+      fr: 'formation',
+    },
+    secondPart: {
+      en: ' and by my ',
+      fr: ' et de ma ',
+    },
+    secondSpan: {
+      en: 'own initiative',
+      fr: 'propre initiative',
+    },
+    lastPart: {
+      en: ', I have acquired professional experience.',
+      fr: ', j’ai pu acquérir de l’expérience en milieu professionnel.',
+    },
+  },
+};
+
+export default explanation;
