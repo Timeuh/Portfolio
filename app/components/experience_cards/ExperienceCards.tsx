3a442a4ebd669544b4555ad60bbef192068c6ead
Timeuh <timeuuh@gmail.com>
2024-03-10 16:39:25 +0100

    :sparkles: add experience cards + some styling

--- app/components/experience_cards/ExperienceCards.tsx
+++ app/components/experience_cards/ExperienceCards.tsx
@@ -1,7 +1,23 @@
+import {experienceSlides} from '@texts/portfolio/experience/experienceSlides';
+import {Slide} from '@appTypes/portfolio';
+import ExperienceCard from '@components/experience_cards/ExperienceCard';
+import {hstack} from '@/styled-system/patterns';
+
 export default function ExperienceCards() {
+  const experienceCardsStyle = hstack({
+    display: {
+      base: 'none',
+      xl: 'flex',
+    },
+    columnGap: 20,
+    width: '90vw',
+  });
+
   return (
-    <section id={'experience'}>
-      <h2>Experience cards</h2>
+    <section id={'experience-cards'} className={experienceCardsStyle}>
+      {experienceSlides.map((slide: Slide, index: number) => {
+        return <ExperienceCard key={index} slide={slide} />;
+      })}
     </section>
   );
 }
