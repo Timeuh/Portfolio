893852e16f6f43637023575173090810c44cdcc1
Timeuh <timeuuh@gmail.com>
2024-03-11 10:35:03 +0100

    :lipstick: move experience cards styles into dedicated file

--- app/components/experience_cards/ExperienceCards.tsx
+++ app/components/experience_cards/ExperienceCards.tsx
@@ -1,17 +1,8 @@
 import ExperienceCard from '@components/experience_cards/ExperienceCard';
-import {hstack} from '@/styled-system/patterns';
 import {experienceSlides} from '@texts/portfolio/experience/experienceSlides';
+import experienceCardsStyle from '@styles/components/experience_cards/ExperienceCards.styles';
 
 export default function ExperienceCards() {
-  const experienceCardsStyle = hstack({
-    display: {
-      base: 'none',
-      xl: 'flex',
-    },
-    columnGap: 20,
-    width: '90vw',
-  });
-
   return (
     <section id={'experience-cards'} className={experienceCardsStyle}>
       <ExperienceCard slide={experienceSlides[1]} cardOrder={'first'}>
---
+++ app/styles/components/experience_cards/ExperienceCards.styles.ts
@@ -0,0 +1,12 @@
+import {hstack} from '@/styled-system/patterns';
+
+const experienceCardsStyle = hstack({
+  display: {
+    base: 'none',
+    xl: 'flex',
+  },
+  columnGap: 20,
+  width: '90vw',
+});
+
+export default experienceCardsStyle;
