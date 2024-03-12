d92d49b202fcf2bcb349b021779138f6fceda60b
Timeuh <timeuuh@gmail.com>
2024-03-12 15:37:05 +0100

    :lipstick: add colors to each icon

--- app/components/activities/Activity.tsx
+++ app/components/activities/Activity.tsx
@@ -24,12 +24,29 @@ const containerStyle = grid({
 });
 
 const svgStyle = css({
-  stroke: {
-    base: 'neutral.800',
-    _dark: 'neutral.200',
-  },
   height: 12,
   width: 12,
+});
+
+const gameIconStyle = css({
+  stroke: {
+    base: 'neutral.700',
+    _dark: 'neutral.200',
+  },
+});
+
+const devIconStyle = css({
+  stroke: {
+    base: 'p_blue.700',
+    _dark: 'p_blue.200',
+  },
+});
+
+const discoveriesIconStyle = css({
+  stroke: {
+    base: 'p_purple.700',
+    _dark: 'p_purple.200',
+  },
 });
 
 const dividerStyle = css({
@@ -112,7 +129,7 @@ Activity.GameIcon = function ActivityGameIcon() {
       stroke='currentColor'
       strokeLinecap='round'
       strokeLinejoin='round'
-      className={svgStyle}
+      className={`${svgStyle} ${gameIconStyle}`}
     >
       <line x1='6' x2='10' y1='11' y2='11' />
       <line x1='8' x2='8' y1='9' y2='13' />
@@ -137,7 +154,7 @@ Activity.DevIcon = function ActivityDevIcon() {
       stroke='currentColor'
       strokeLinecap='round'
       strokeLinejoin='round'
-      className={svgStyle}
+      className={`${svgStyle} ${devIconStyle}`}
     >
       <rect width='18' height='18' x='3' y='3' rx='2' />
       <path d='m10 10-2 2 2 2' />
@@ -160,7 +177,7 @@ Activity.DiscoveriesIcon = function ActivityDiscoveriesIcon() {
       stroke='currentColor'
       strokeLinecap='round'
       strokeLinejoin='round'
-      className={svgStyle}
+      className={`${svgStyle} ${discoveriesIconStyle}`}
     >
       <path d='M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H20v20H6.5a2.5 2.5 0 0 1 0-5H20' />
       <polyline points='10 2 10 10 13 7 16 10 16 2' />
