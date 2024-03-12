49a3de181493a0950b91247c9f6046f4e21c3993
Timeuh <timeuuh@gmail.com>
2024-03-12 16:23:49 +0100

    :speech_balloon: create texts for activity section (title and link)

---
+++ app/texts/portfolio/activity/activityTexts.ts
@@ -0,0 +1,14 @@
+import {ActivityTexts} from '@appTypes/portfolio';
+
+const activityTexts: ActivityTexts = {
+  title: {
+    en: 'My activities',
+    fr: 'Mes activités',
+  },
+  link: {
+    en: 'See my projects',
+    fr: 'Voir mes projets',
+  },
+};
+
+export default activityTexts;
