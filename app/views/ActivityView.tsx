66b9749eee8dd7ef4c90665f626299331b0a6683
Timeuh <timeuuh@gmail.com>
2024-03-12 16:23:59 +0100

    :recycle: use activity section texts

--- app/views/ActivityView.tsx
+++ app/views/ActivityView.tsx
@@ -1,14 +1,20 @@
+'use client';
+
 import Link from 'next/link';
 import Activities from '@components/activities/Activities';
 import {activityStyle, projectsLinkStyle, titleStyle} from '@styles/views/ActivityView.styles';
+import useLanguage from '@hooks/useLanguage';
+import activityTexts from '@texts/portfolio/activity/activityTexts';
 
 export default function ActivityView() {
+  const {langage} = useLanguage();
+
   return (
     <section id={'activity'} className={activityStyle}>
-      <h2 className={titleStyle}>Mes activités</h2>
+      <h2 className={titleStyle}>{activityTexts.title[langage]}</h2>
       <Activities />
       <Link href={'/projects'} className={projectsLinkStyle}>
-        Voir mes projets
+        {activityTexts.link[langage]}
       </Link>
     </section>
   );
