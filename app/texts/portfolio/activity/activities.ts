897e8042685635e86d330dfa0c5f8a02a79d0832
Timeuh <timeuuh@gmail.com>
2024-03-12 14:06:37 +0100

    :speech_balloon: create texts for activities

---
+++ app/texts/portfolio/activity/activities.ts
@@ -0,0 +1,78 @@
+import {Activity} from '@appTypes/portfolio';
+
+const activities: Activity[] = [
+  {
+    title: {
+      en: 'Video games',
+      fr: 'Jeux vidéo',
+    },
+    description: {
+      firstPart: {
+        en: 'My main hobby is playing video games. I like several types of games, whether solo or multiplayer. My favorite games are',
+        fr: 'Mon principal hobby est de jouer aux jeux vidéos. J’aime plusieurs type de jeux, que ce soit en solo ou multijoueur. Mes jeux préférés sont',
+      },
+      firstSpan: {
+        en: ' Minecraft ',
+        fr: ' Minecraft ',
+      },
+      secondPart: {
+        en: 'and ',
+        fr: 'et ',
+      },
+      secondSpan: {
+        en: 'The Binding of Isaac.',
+        fr: 'The Binding of Isaac.',
+      },
+    },
+  },
+  {
+    title: {
+      en: 'Development',
+      fr: 'Développement',
+    },
+    description: {
+      firstPart: {
+        en: 'Outside of video games, I love to develop small or large projects on my own, to learn',
+        fr: 'En dehors des jeux vidéo, j’adore développer des petits ou gros projets de mon côté, pour apprendre de',
+      },
+      firstSpan: {
+        en: ' new technologies',
+        fr: ' nouvelles technologies',
+      },
+      secondPart: {
+        en: '. My most recent discovery is ',
+        fr: '. Ma découverte la plus récente est ',
+      },
+      secondSpan: {
+        en: 'Next Js.',
+        fr: 'Next Js.',
+      },
+    },
+  },
+  {
+    title: {
+      en: 'Discoveries',
+      fr: 'Découvertes',
+    },
+    description: {
+      firstPart: {
+        en: 'I love to discover new things, especially in',
+        fr: 'J’adore découvrir de nouvelles choses, surtout dans le',
+      },
+      firstSpan: {
+        en: ' web development',
+        fr: ' développement web',
+      },
+      secondPart: {
+        en: ', but also through my various readings. I read a lot of ',
+        fr: ', mais aussi à travers mes différentes lectures. Je lis beaucoup de ',
+      },
+      secondSpan: {
+        en: 'novels, mangas and webtoons.',
+        fr: 'romans, de mangas et de webtoons.',
+      },
+    },
+  },
+];
+
+export default activities;
