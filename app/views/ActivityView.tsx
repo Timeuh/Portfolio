9c73260a4a0db76e931760feb617023125d2271d
Timeuh <timeuuh@gmail.com>
2024-03-12 16:56:11 +0100

    :truck: move Activities display into ActivityView (is now client component as well)

--- app/views/ActivityView.tsx
+++ app/views/ActivityView.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import Link from 'next/link';
-import Activities from '@components/activities/Activities';
 import {activityStyle, projectsLinkStyle, titleStyle} from '@styles/views/ActivityView.styles';
 import useLanguage from '@hooks/useLanguage';
 import activityTexts from '@texts/portfolio/activity/activityTexts';
+import activities from '@texts/portfolio/activity/activities';
+import Link from 'next/link';
+import Activity from '@components/activities/Activity';
 
 export default function ActivityView() {
   const {langage} = useLanguage();
@@ -12,7 +13,21 @@ export default function ActivityView() {
   return (
     <section id={'activity'} className={activityStyle}>
       <h2 className={titleStyle}>{activityTexts.title[langage]}</h2>
-      <Activities />
+      <Activity activity={activities[0]} variant={'game'}>
+        <Activity.Heading title={activities[0].title} variant={'game'}>
+          <Activity.GameIcon />
+        </Activity.Heading>
+      </Activity>
+      <Activity activity={activities[1]} variant={'dev'}>
+        <Activity.Heading title={activities[1].title} variant={'dev'}>
+          <Activity.DevIcon />
+        </Activity.Heading>
+      </Activity>
+      <Activity activity={activities[2]} variant={'discoveries'}>
+        <Activity.Heading title={activities[2].title} variant={'discoveries'}>
+          <Activity.DiscoveriesIcon />
+        </Activity.Heading>
+      </Activity>
       <Link href={'/projects'} className={projectsLinkStyle}>
         {activityTexts.link[langage]}
       </Link>
