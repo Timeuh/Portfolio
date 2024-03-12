77cf0e0315e6ffcbf8a96b2874caf8e1808d7eb6
Timeuh <timeuuh@gmail.com>
2024-03-12 16:08:32 +0100

    :recycle: move styles in dedicated file

---
+++ app/styles/views/ActivityView.styles.tsx
@@ -0,0 +1,42 @@
+import {vstack} from '@/styled-system/patterns';
+import {css} from '@/styled-system/css';
+
+const activityStyle = vstack({
+  bgColor: {
+    base: 'p_purple.light',
+    _dark: 'p_purple.dark',
+  },
+  gap: 12,
+  pb: 10,
+});
+
+const titleStyle = css({
+  color: {
+    base: 'neutral.800',
+    _dark: 'neutral.200',
+  },
+  fontFamily: 'bricolage',
+  fontSize: '4xl',
+  fontWeight: 'bold',
+  textAlign: 'center',
+});
+
+const projectsLinkStyle = css({
+  bgColor: {
+    base: 'p_purple.dark',
+    _dark: 'p_purple.light',
+  },
+  borderRadius: 'md',
+  color: {
+    base: 'neutral.light',
+    _dark: 'neutral.dark',
+  },
+  fontFamily: 'bricolage',
+  fontSize: '2xl',
+  fontWeight: 'bold',
+  p: 2,
+  textAlign: 'center',
+  w: '4/5',
+});
+
+export {activityStyle, titleStyle, projectsLinkStyle};
--- app/views/ActivityView.tsx
+++ app/views/ActivityView.tsx
@@ -1,45 +1,6 @@
-import {css} from '@/styled-system/css';
-import {vstack} from '@/styled-system/patterns';
 import Link from 'next/link';
 import Activities from '@components/activities/Activities';
-
-const activityStyle = vstack({
-  bgColor: {
-    base: 'p_purple.light',
-    _dark: 'p_purple.dark',
-  },
-  gap: 12,
-  pb: 10,
-});
-
-const titleStyle = css({
-  color: {
-    base: 'neutral.800',
-    _dark: 'neutral.200',
-  },
-  fontFamily: 'bricolage',
-  fontSize: '4xl',
-  fontWeight: 'bold',
-  textAlign: 'center',
-});
-
-const projectsLinkStyle = css({
-  bgColor: {
-    base: 'p_purple.dark',
-    _dark: 'p_purple.light',
-  },
-  borderRadius: 'md',
-  color: {
-    base: 'neutral.light',
-    _dark: 'neutral.dark',
-  },
-  fontFamily: 'bricolage',
-  fontSize: '2xl',
-  fontWeight: 'bold',
-  p: 2,
-  textAlign: 'center',
-  w: '4/5',
-});
+import {activityStyle, projectsLinkStyle, titleStyle} from '@styles/views/ActivityView.styles';
 
 export default function ActivityView() {
   return (
