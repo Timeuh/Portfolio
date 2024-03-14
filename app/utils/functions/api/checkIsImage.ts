e370393084d773902fca5e7ca9e553126216c529
Timeuh <timeuuh@gmail.com>
2024-03-14 15:40:56 +0100

    :sparkles: add gif check in image check function

--- app/utils/functions/api/checkIsImage.ts
+++ app/utils/functions/api/checkIsImage.ts
@@ -6,7 +6,7 @@
  * @returns {boolean} true if the name is a valid image name, false otherwise
  */
 const checkIsImage = (imageName: string): boolean => {
-  const regex = /\.(jpg|jpeg|png)$/i;
+  const regex = /\.(jpg|jpeg|png|gif)$/i;
   return regex.test(imageName);
 };
 
