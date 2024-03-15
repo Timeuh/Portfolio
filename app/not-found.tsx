d0ad78bd2e21ad20281dd96ce0b371d6b3337abf
Timeuh <timeuuh@gmail.com>
2024-03-15 14:11:59 +0100

    :sparkles: create not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+import {Metadata} from 'next';
+
+export const metadata: Metadata = {
+  title: 'Timeuh - Not Found',
+};
+
+export default function NotFound() {
+  return (
+    <main>
+      <h1>Not Found</h1>
+    </main>
+  );
+}
