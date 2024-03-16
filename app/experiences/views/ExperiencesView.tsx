307298bf89e0d470bb266d556a1031492598d1f7
Timeuh <timeuuh@gmail.com>
2024-03-16 11:14:33 +0100

    :sparkles: get and log experiences from hook (WIP)

--- app/experiences/views/ExperiencesView.tsx
+++ app/experiences/views/ExperiencesView.tsx
@@ -1,4 +1,14 @@
+'use client';
+
+import useExperiences from '@app/experiences/hooks/useExperiences';
+
 export default function ExperiencesView() {
+  const {data, isLoading} = useExperiences();
+
+  if (!isLoading && data) {
+    console.log(data);
+  }
+
   return (
     <div>
       <h1>Experiences</h1>
