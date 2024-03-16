0406e6a42b6610006ceae5203af01140ad934aa6
Timeuh <timeuuh@gmail.com>
2024-03-16 11:32:49 +0100

    :sparkles: create sections for each experience and display its company name

--- app/experiences/views/ExperiencesView.tsx
+++ app/experiences/views/ExperiencesView.tsx
@@ -1,18 +1,39 @@
 'use client';
 
 import useExperiences from '@app/experiences/hooks/useExperiences';
+import {vstack} from '@/styled-system/patterns';
 
 export default function ExperiencesView() {
   const {data, isLoading} = useExperiences();
 
-  if (!isLoading && data) {
-    console.log(data);
+  if (!data) {
+    return;
   }
+
+  const expContainerStyle = vstack({
+    bgColor: {
+      base: 'p_purple.light',
+      _dark: 'p_purple.dark',
+    },
+    color: {
+      base: 'neutral.dark',
+      _dark: 'neutral.light',
+    },
+    h: 'screen',
+    justifyContent: 'center',
+  });
 
   return (
     <section id={'experiences-display'}>
-      <h1>Experiences</h1>
-      <p>Work experiences</p>
+      <div id={'first-exp'} className={expContainerStyle}>
+        {data.items[0].company}
+      </div>
+      <div id={'second-exp'} className={expContainerStyle}>
+        {data.items[1].company}
+      </div>
+      <div id={'third-exp'} className={expContainerStyle}>
+        {data.items[2].company}
+      </div>
     </section>
   );
 }
