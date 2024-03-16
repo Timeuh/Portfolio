9a325b0d4f72eb8f426277bb8d27f00686b38683
Timeuh <timeuuh@gmail.com>
2024-03-16 10:08:01 +0100

    :speech_balloon: create experience page landing section texts

---
+++ app/experiences/texts/landingTexts.ts
@@ -0,0 +1,18 @@
+import {ExperienceLandingTexts} from '@appTypes/portfolio';
+
+const landingTexts: ExperienceLandingTexts = {
+  title: {
+    en: 'My Experiences',
+    fr: 'Mes Experiences',
+  },
+  explanations: {
+    en: 'During my studies and my personal transition time (school holidays in this case), I was able to accumulate work experience. You can see these below.',
+    fr: 'Durant mes études et mon temps personnel de transition (les vacances scolaires en l’occurrence), j’ai pu accumuler de l’expérience au travail. Vous pouvez voir ces dernières ci-dessous.',
+  },
+  link: {
+    en: 'Discover',
+    fr: 'Découvrir',
+  },
+};
+
+export default landingTexts;
