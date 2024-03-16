d295319f6b5ce41641daf5921db8c0f58e2c589b
Timeuh <timeuuh@gmail.com>
2024-03-16 09:05:29 +0100

    :speech_balloon: create not found page title and link texts

---
+++ app/texts/portfolio/not_found/titleAndLink.ts
@@ -0,0 +1,14 @@
+import {NotFoundTexts} from '@appTypes/portfolio';
+
+const titleAndLink: NotFoundTexts = {
+  title: {
+    en: 'Sorry, this page does not exist !',
+    fr: 'Désolé, cette page n’existe pas !',
+  },
+  link: {
+    en: 'Back to home',
+    fr: 'Retour à l’accueil',
+  },
+};
+
+export default titleAndLink;
