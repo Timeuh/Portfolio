3f1fd5fe5786d3277de3d585d596562a28e9bb94
Timeuh <timeuuh@gmail.com>
2024-03-16 09:05:46 +0100

    :recycle: use title and link texts for not found page

--- app/views/NotFoundView.tsx
+++ app/views/NotFoundView.tsx
@@ -1,12 +1,18 @@
+'use client';
+
 import Link from 'next/link';
 import {notFoundStyle, returnToHomeStyle, titleStyle} from '@styles/views/NotFoundView.styles';
+import useLanguage from '@hooks/useLanguage';
+import titleAndLink from '@texts/portfolio/not_found/titleAndLink';
 
 export default function NotFoundView() {
+  const {langage} = useLanguage();
+
   return (
     <main className={notFoundStyle}>
-      <h1 className={titleStyle}>Désolé, cette page n&apos;existe pas !</h1>
+      <h1 className={titleStyle}>{titleAndLink.title[langage]}</h1>
       <Link href={'/'} className={returnToHomeStyle}>
-        Retour à l&apos;accueil
+        {titleAndLink.link[langage]}
       </Link>
     </main>
   );
