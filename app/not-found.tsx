a316dbf2b0a6615b60ce94486185bc58a16532aa
Timeuh <timeuuh@gmail.com>
2024-03-16 10:12:51 +0100

    :sparkles: add not found page metadata description

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,6 +3,7 @@ import NotFoundView from '@views/NotFoundView';
 
 export const metadata: Metadata = {
   title: 'Timeuh - Page introuvable',
+  description: 'Not found page for Timeuh portfolio',
 };
 
 export default function NotFound() {
