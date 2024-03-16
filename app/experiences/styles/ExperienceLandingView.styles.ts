1658510ea458471085ec9e8595bb05e14f215fc2
Timeuh <timeuuh@gmail.com>
2024-03-16 10:21:21 +0100

    :lipstick: adapt experience landing view to tablet format

--- app/experiences/styles/ExperienceLandingView.styles.ts
+++ app/experiences/styles/ExperienceLandingView.styles.ts
@@ -10,32 +10,47 @@ const experiencesLandingStyle = vstack({
     base: 'neutral.dark',
     _dark: 'neutral.light',
   },
-  gap: 8,
+  gap: {
+    base: 8,
+    md: 12,
+  },
   h: 'screen',
   justifyContent: 'center',
   transition: 'all 0.5s',
 });
 
 const textContainerStyle = vstack({
-  gap: 8,
+  gap: {
+    base: 8,
+    md: 12,
+  },
   justifyContent: 'center',
-  px: 3,
+  px: {
+    base: 3,
+    md: 8,
+  },
 });
 
 const imageStyle: string = css({
   borderRadius: '2xl',
-  height: '250px',
+  height: {
+    base: '250px',
+    md: '350px',
+  },
   objectFit: 'cover',
   shadow: {
     base: '4px 4px 5px 2px token(colors.p_blue.dark_25)',
     _dark: '4px 4px 5px 2px token(colors.p_blue.light_25)',
   },
-  width: '80%',
+  width: '50%',
 });
 
 const titleStyle = css({
   fontFamily: 'bricolage',
-  fontSize: '4xl',
+  fontSize: {
+    base: '4xl',
+    md: '6xl',
+  },
   fontWeight: 'bold',
 });
 
@@ -44,16 +59,27 @@ const paragraphStyle = css({
     base: 'neutral.800',
     _dark: 'neutral.200',
   },
-  fontSize: '1.4em',
+  fontSize: {
+    base: '1.4em',
+    md: '4xl',
+  },
   textAlign: 'center',
 });
 
 const svgStyle = css({
+  h: {
+    base: 7,
+    md: 12,
+  },
   stroke: {
     base: 'neutral.light',
     _dark: 'neutral.dark',
   },
   strokeWidth: 4,
+  w: {
+    base: 7,
+    md: 12,
+  },
 });
 
 const linkToNextStyle = hstack({
@@ -66,11 +92,24 @@ const linkToNextStyle = hstack({
     base: 'neutral.light',
     _dark: 'neutral.dark',
   },
-  fontSize: '2xl',
+  fontSize: {
+    base: '2xl',
+    md: '4xl',
+  },
   fontWeight: 'semibold',
+  gap: {
+    base: 2,
+    md: 4,
+  },
   justifyContent: 'center',
-  p: 2,
-  w: '4/5',
+  p: {
+    base: 2,
+    md: 4,
+  },
+  w: {
+    base: '4/5',
+    md: '1/2',
+  },
 });
 
 export {experiencesLandingStyle, textContainerStyle, imageStyle, titleStyle, paragraphStyle, svgStyle, linkToNextStyle};
