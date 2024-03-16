7db72b668bbae0322ebcf98a221e4431a724e09f
Timeuh <timeuuh@gmail.com>
2024-03-16 08:49:28 +0100

    :sparkles: add content and styles to not found page

--- app/views/NotFoundView.tsx
+++ app/views/NotFoundView.tsx
@@ -1,7 +1,51 @@
+import {vstack} from '@/styled-system/patterns';
+import {css} from '@/styled-system/css';
+import Link from 'next/link';
+
 export default function NotFoundView() {
+  const notFoundStyle = vstack({
+    bgColor: {
+      base: 'p_purple.light',
+      _dark: 'p_purple.dark',
+    },
+    color: {
+      base: 'neutral.dark',
+      _dark: 'neutral.light',
+    },
+    gap: 12,
+    h: '92.3vh',
+    justifyContent: 'center',
+    transition: 'background-color 0.5s',
+  });
+
+  const titleStyle = css({
+    fontFamily: 'bricolage',
+    fontSize: '3xl',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  });
+
+  const returnToHomeStyle = css({
+    bgColor: {
+      base: 'p_blue.dark',
+      _dark: 'p_blue.light',
+    },
+    borderRadius: 'lg',
+    color: {
+      base: 'neutral.light',
+      _dark: 'neutral.dark',
+    },
+    fontSize: 'lg',
+    p: 3,
+    textAlign: 'center',
+  });
+
   return (
-    <main>
-      <h1>Not Found</h1>
+    <main className={notFoundStyle}>
+      <h1 className={titleStyle}>Désolé, cette page n&apos;existe pas !</h1>
+      <Link href={'/'} className={returnToHomeStyle}>
+        Retour à l&apos;accueil
+      </Link>
     </main>
   );
 }
