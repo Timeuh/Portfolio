bfe729a3e16fd92d8b31df5e5bdda7187c8b325f
Timeuh <timeuuh@gmail.com>
2024-03-16 10:11:55 +0100

    :sparkles: add experience page metadata

--- app/experiences/page.tsx
+++ app/experiences/page.tsx
@@ -1,4 +1,10 @@
 import ExperiencesLandingView from '@app/experiences/views/ExperiencesLandingView';
+import {Metadata} from 'next';
+
+export const metadata: Metadata = {
+  title: 'Timeuh - Experiences',
+  description: 'Page which displays Timeuh work experiences.',
+};
 
 export default function Page() {
   return (
