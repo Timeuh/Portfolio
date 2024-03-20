926f4b13474854a4910ed172be612fe43fd1ac02
Timeuh <timeuuh@gmail.com>
2024-03-20 14:12:19 +0100

    :sparkles: return loading experience or actual experience depending on the isLoading state

--- app/experiences/components/ExperienceFactory.tsx
+++ app/experiences/components/ExperienceFactory.tsx
@@ -1,4 +1,6 @@
 import {CompleteExperienceFromApi} from '@schemas/api/experience/experience.schema';
+import useLanguage from '@hooks/useLanguage';
+import Experience from '@app/experiences/components/Experience';
 
 type Props = {
   experience: CompleteExperienceFromApi;
@@ -6,17 +8,29 @@ type Props = {
 };
 
 export default function ExperienceFactory({experience, isLoading}: Props) {
+  const {language} = useLanguage();
+
   if (isLoading) {
     return (
-      <div>
-        <h1>Loading</h1>
-      </div>
+      <Experience experience={experience}>
+        <Experience.TextContainer>
+          <Experience.LoadingCompany />
+          <Experience.LoadingJob />
+          <Experience.LoadingTechnologies />
+        </Experience.TextContainer>
+        <Experience.LoadingLogoAndPeriod />
+      </Experience>
     );
   }
 
   return (
-    <div>
-      <h1>{experience.company}</h1>
-    </div>
+    <Experience experience={experience}>
+      <Experience.TextContainer>
+        <Experience.Company language={language} experience={experience} />
+        <Experience.Job language={language} experience={experience} />
+        <Experience.Technologies language={language} experience={experience} />
+      </Experience.TextContainer>
+      <Experience.LogoAndPeriod language={language} experience={experience} />
+    </Experience>
   );
 }
