62dcd6ef197c97a99f8f61a40d9e358c688f56b3
Timeuh <timeuuh@gmail.com>
2024-03-21 14:29:33 +0100

    :lipstick: style experience content (WIP, mobile only)

--- app/experiences/components/Experience.tsx
+++ app/experiences/components/Experience.tsx
@@ -3,8 +3,9 @@ import React, {ReactNode} from 'react';
 import {Language} from '@appTypes/portfolio';
 import {Technology} from '@prisma/client';
 import Image from 'next/image';
-import {hstack, vstack} from '@/styled-system/patterns';
+import {circle, grid, hstack, vstack} from '@/styled-system/patterns';
 import {css} from '@/styled-system/css';
+import {API_TECH_LOGO_URL} from '@constants/portfolio';
 
 type Props = {
   experience: CompleteExperienceFromApi;
@@ -73,7 +74,7 @@ const experienceStyle = css({
     xl: 8,
   },
   h: '80vh',
-  justifyContent: 'center',
+  overflow: 'auto',
   p: {
     base: 6,
     md: 8,
@@ -85,7 +86,7 @@ const experienceStyle = css({
   transition: 'all 0.5s',
 });
 
-const experienceContainerStyle = css({
+const loadingContainerStyle = css({
   display: 'flex',
   flexDirection: 'column',
   gap: {
@@ -171,6 +172,104 @@ const companyTitleStyle = css({
   },
 });
 
+const companyNameStyle = css({
+  font: 'bricolage',
+  fontSize: '3xl',
+  fontWeight: 'bold',
+});
+
+const expHeadingStyle = css({
+  font: 'bricolage',
+  fontSize: 'xl',
+  fontWeight: 'bold',
+});
+
+const paragraphStyle = css({
+  fontSize: 'md',
+});
+
+const textContainerStyle = vstack({
+  alignItems: 'start',
+  gap: 2,
+});
+
+const experienceContainerStyle = css({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  gap: 4,
+});
+
+const techItemStyle = vstack({
+  bgColor: {
+    base: 'p_blue.300',
+    _dark: 'p_blue.800',
+  },
+  borderRadius: 'md',
+  fontSize: '15px',
+  justifyContent: 'center',
+  p: 1,
+  w: 20,
+  h: '15vh',
+  textAlign: 'center',
+});
+
+const techContainerStyle = grid({
+  gridTemplateColumns: 3,
+  gap: 4,
+});
+
+const techImageContainer = circle({
+  bgColor: {
+    base: 'p_blue.light',
+    _dark: 'p_blue.dark',
+  },
+  size: 14,
+});
+
+const techImageStyle = css({
+  w: 10,
+  h: 10,
+  objectFit: 'cover',
+});
+
+const techsContainerStyle = vstack({
+  alignItems: 'start',
+  gap: 4,
+});
+
+const companyLogoStyle = css({
+  h: 24,
+  w: 32,
+});
+
+const companyLogoAndPeriodStyle = hstack({
+  gap: 4,
+});
+
+const companyLogoContainerStyle = vstack({
+  bgColor: {
+    base: 'neutral.light',
+    _dark: 'neutral.dark',
+  },
+  border: '2px solid',
+  borderColor: {
+    base: 'neutral.dark',
+    _dark: 'neutral.light',
+  },
+  borderRadius: 'md',
+  justifyContent: 'center',
+  h: 36,
+  w: 36,
+  p: 2,
+});
+
+const jobPeriodStyle = css({
+  fontWeight: 'bold',
+  textAlign: 'center',
+  w: 'fit',
+});
+
 export default function Experience({experience, children}: Props) {
   return (
     <div id={`experience-${experience.id}`} className={experienceStyle}>
@@ -180,14 +279,18 @@ export default function Experience({experience, children}: Props) {
 }
 
 Experience.TextContainer = function ExperienceTextContainer({children}: {children: ReactNode}) {
-  return <div className={`experience-container ${experienceContainerStyle}`}>{children}</div>;
+  return <div className={experienceContainerStyle}>{children}</div>;
+};
+
+Experience.LoadingContainer = function ExperienceLoadingContainer({children}: {children: ReactNode}) {
+  return <div className={`experience-container ${loadingContainerStyle}`}>{children}</div>;
 };
 
 Experience.Company = function ExperienceCompany({experience, language}: CompositionProps) {
   return (
-    <div>
-      <h2>{experience.company}</h2>
-      <p>{experience.description[language]}</p>
+    <div className={textContainerStyle}>
+      <h2 className={companyNameStyle}>{experience.company}</h2>
+      <p className={paragraphStyle}>{experience.description[language]}</p>
     </div>
   );
 };
@@ -203,9 +306,9 @@ Experience.LoadingCompany = function ExperienceLoadingCompany() {
 
 Experience.Job = function ExperienceJob({experience, language}: CompositionProps) {
   return (
-    <div>
-      <h3>{experience.job_title[language]}</h3>
-      <p>{experience.job_description[language]}</p>
+    <div className={textContainerStyle}>
+      <h3 className={expHeadingStyle}>{experience.job_title[language]}</h3>
+      <p className={paragraphStyle}>{experience.job_description[language]}</p>
     </div>
   );
 };
@@ -221,11 +324,27 @@ Experience.LoadingJob = function ExperienceLoadingJob() {
 
 Experience.Technologies = function ExperienceTechnologies({experience, language}: CompositionProps) {
   return (
-    <div>
-      <h3>Les technologies utilisées</h3>
-      {experience.technologies.map((technology: Technology, index: number) => {
-        return <p key={index}>{technology.name}</p>;
-      })}
+    <div className={techsContainerStyle}>
+      <h3 className={expHeadingStyle}>Les technologies utilisées</h3>
+      <div className={techContainerStyle}>
+        {experience.technologies.map((technology: Technology, index: number) => {
+          return (
+            <div key={index} className={techItemStyle}>
+              <div className={techImageContainer}>
+                <Image
+                  src={API_TECH_LOGO_URL + technology.logo}
+                  alt={technology.name}
+                  width={0}
+                  height={0}
+                  sizes={'100vw'}
+                  className={techImageStyle}
+                />
+              </div>
+              <p>{technology.name}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
@@ -243,12 +362,25 @@ Experience.LoadingTechnologies = function ExperienceLoadingTechnologies() {
 };
 
 Experience.LogoAndPeriod = function ExperienceLogoAndPeriod({experience, language}: CompositionProps) {
+  const startDate = new Date(experience.start_date);
+  const endDate = new Date(experience.end_date);
+
+  const formattedStart = new Intl.DateTimeFormat('fr-FR').format(startDate);
+  const formattedEnd = new Intl.DateTimeFormat('fr-FR').format(endDate);
+
   return (
-    <div>
-      <Image src={experience.links.logo.href} alt={experience.company} width={0} height={0} sizes={'100vw'} />
-      <h3>
-        {new Date(experience.start_date).toLocaleDateString()} - {new Date(experience.end_date).toLocaleDateString()}
-      </h3>
+    <div className={companyLogoAndPeriodStyle}>
+      <div className={companyLogoContainerStyle}>
+        <Image
+          src={experience.links.logo.href}
+          alt={experience.company}
+          width={0}
+          height={0}
+          sizes={'100vw'}
+          className={companyLogoStyle}
+        />
+      </div>
+      <h3 className={jobPeriodStyle}>{`${formattedStart} - ${formattedEnd}`}</h3>
     </div>
   );
 };
