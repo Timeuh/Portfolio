959a6538ea62cc2eefc0ba136e816b3d5af585b3
Timeuh <timeuuh@gmail.com>
2024-03-22 11:36:58 +0100

    :recycle: remove type from useContext call

--- app/experiences/hooks/useExperience.tsx
+++ app/experiences/hooks/useExperience.tsx
@@ -1,12 +1,11 @@
 import {ExperienceContext} from '@app/experiences/providers/ExperienceProvider';
 import {useContext} from 'react';
-import {CompleteExperienceFromApi} from '@schemas/api/experience/experience.schema';
 
 /**
  * Custom hook to get the experience object for Experience components
  */
 const useExperience = () => {
-  const experienceContext = useContext<CompleteExperienceFromApi | null>(ExperienceContext);
+  const experienceContext = useContext(ExperienceContext);
   if (!experienceContext) {
     throw new Error('useExperience must be used within an ExperienceProvider');
   }
