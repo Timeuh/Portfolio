e85917e9f25ca75b855752b2aef37366b02b7bca
Timeuh <timeuuh@gmail.com>
2024-03-22 11:37:16 +0100

    :sparkles: also provide language with context

--- app/experiences/providers/ExperienceProvider.tsx
+++ app/experiences/providers/ExperienceProvider.tsx
@@ -1,13 +1,15 @@
 import {createContext, ReactNode} from 'react';
 import {CompleteExperienceFromApi} from '@schemas/api/experience/experience.schema';
+import {ExperienceContextUtils, Language} from '@appTypes/portfolio';
 
-export const ExperienceContext = createContext<null | CompleteExperienceFromApi>(null);
+export const ExperienceContext = createContext<null | ExperienceContextUtils>(null);
 
 type Props = {
   children: ReactNode;
   experience: CompleteExperienceFromApi;
+  language: Language;
 };
 
-export default function ExperienceProvider({children, experience}: Props) {
-  return <ExperienceContext.Provider value={experience}>{children}</ExperienceContext.Provider>;
+export default function ExperienceProvider({children, experience, language}: Props) {
+  return <ExperienceContext.Provider value={{experience, language}}>{children}</ExperienceContext.Provider>;
 }
