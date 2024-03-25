0457fd88dbe21b6328e3e52d719748371fa1303d
Timeuh <timeuuh@gmail.com>
2024-03-25 14:02:11 +0100

    :recycle: move sidebar

--- app/experiences/page.tsx
+++ app/experiences/page.tsx
@@ -2,7 +2,6 @@ import ExperiencesLandingView from './views/landing/ExperiencesLandingView';
 import ExperiencesView from './views/experiences/ExperiencesView';
 import ExperiencesEndView from './views/end/ExperiencesEndView';
 import {Metadata} from 'next';
-import ExperiencesSidebar from '@app/experiences/components/sidebar/sidebar/ExperiencesSidebar';
 
 export const metadata: Metadata = {
   title: 'Timeuh - Experiences',
@@ -12,7 +11,6 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <main>
-      <ExperiencesSidebar />
       <ExperiencesLandingView />
       <ExperiencesView />
       <ExperiencesEndView />
