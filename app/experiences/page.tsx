1a2ad2defaf73111de0494ad87de838d1c582795
Timeuh <timeuuh@gmail.com>
2024-03-25 12:02:43 +0100

    :sparkles: use experience sidebar component

--- app/experiences/page.tsx
+++ app/experiences/page.tsx
@@ -2,6 +2,7 @@ import ExperiencesLandingView from './views/landing/ExperiencesLandingView';
 import ExperiencesView from './views/experiences/ExperiencesView';
 import ExperiencesEndView from './views/end/ExperiencesEndView';
 import {Metadata} from 'next';
+import ExperiencesSidebar from '@app/experiences/components/sidebar/sidebar/ExperiencesSidebar';
 
 export const metadata: Metadata = {
   title: 'Timeuh - Experiences',
@@ -11,6 +12,7 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <main>
+      <ExperiencesSidebar />
       <ExperiencesLandingView />
       <ExperiencesView />
       <ExperiencesEndView />
