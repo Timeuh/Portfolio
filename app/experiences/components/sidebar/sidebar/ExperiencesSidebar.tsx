4c0615fc07b0d9cd6565249106b59bce977953b1
Timeuh <timeuuh@gmail.com>
2024-04-09 21:07:57 +0200

    :sparkles: change sidebar active tab with current scroll position

--- app/experiences/components/sidebar/sidebar/ExperiencesSidebar.tsx
+++ app/experiences/components/sidebar/sidebar/ExperiencesSidebar.tsx
@@ -1,7 +1,14 @@
+'use client';
+
 import {vstack} from '@/styled-system/patterns';
 import {css} from '@/styled-system/css';
+import {experienceSlides} from '@texts/portfolio/experience/experienceSlides';
+import {Slide} from '@appTypes/portfolio';
+import useScrollPosition from '../../../hooks/useScrollPosition';
 
 export default function ExperiencesSidebar() {
+  const currentActive = useScrollPosition();
+
   const cExpSidebar_sidebar = vstack({
     alignItems: 'end',
     gap: 12,
@@ -15,21 +22,33 @@ export default function ExperiencesSidebar() {
   });
 
   const cExpSidebar_sidebarItem = css({
-    bg: {
-      base: 'neutral.200',
-      _dark: 'neutral.700',
-    },
     borderRadius: 'xl',
     h: '15vh',
     transition: 'all 0.5s',
     w: 4,
   });
 
+  const cExpSidebar_sidebarItemActive = (index: number, currentActive: number) => {
+    const isActive = index === currentActive;
+
+    return css({
+      bg: {
+        base: isActive ? 'p_blue.700' : 'neutral.200',
+        _dark: isActive ? 'p_blue.200' : 'neutral.700',
+      },
+    });
+  };
+
   return (
     <aside id={'sidebar'} className={cExpSidebar_sidebar}>
-      <div className={cExpSidebar_sidebarItem} />
-      <div className={cExpSidebar_sidebarItem} />
-      <div className={cExpSidebar_sidebarItem} />
+      {experienceSlides.map((slide: Slide, index: number) => {
+        return (
+          <div
+            key={index}
+            className={`${cExpSidebar_sidebarItem} ${cExpSidebar_sidebarItemActive(index, currentActive)}`}
+          />
+        );
+      })}
     </aside>
   );
 }
