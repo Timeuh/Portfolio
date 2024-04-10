eac26dc124a929ef0ca747dc5089cfb86bd17203
Timeuh <timeuuh@gmail.com>
2024-04-10 10:21:19 +0200

    :lipstick: make sidebar item tooltip appear on hover

--- app/experiences/components/sidebar/sidebar_item/ExperiencesSidebarItem.tsx
+++ app/experiences/components/sidebar/sidebar_item/ExperiencesSidebarItem.tsx
@@ -8,14 +8,16 @@ type Props = {
 };
 
 export default function ExperiencesSidebarItem({index, currentActive, exp}: Props) {
-  const cExpSidebar_sidebarItem = css({
+  const cExpSidebarItem_item = css({
     borderRadius: 'xl',
+    cursor: 'pointer',
     h: '15vh',
+    position: 'relative',
     transition: 'all 0.5s',
     w: 4,
   });
 
-  const cExpSidebar_sidebarItemActive = (index: number, currentActive: number) => {
+  const cExpSidebarItem_activeState = (index: number, currentActive: number) => {
     const isActive = index === currentActive;
 
     return css({
@@ -23,12 +25,40 @@ export default function ExperiencesSidebarItem({index, currentActive, exp}: Prop
         base: isActive ? 'p_blue.700' : 'neutral.200',
         _dark: isActive ? 'p_blue.200' : 'neutral.700',
       },
+      color: {
+        base: isActive ? 'neutral.light' : 'neutral.700',
+        _dark: isActive ? 'neutral.dark' : 'neutral.light',
+      },
+      transition: 'all 0.5s',
+    });
+  };
+
+  const cExpSidebarItem_title = (index: number, currentActive: number) => {
+    const isActive = index === currentActive;
+
+    return css({
+      bg: {
+        base: isActive ? 'p_blue.700' : 'neutral.200',
+        _dark: isActive ? 'p_blue.200' : 'neutral.700',
+      },
+      borderRadius: 'lg',
+      display: {
+        base: 'none',
+        _groupHover: 'block',
+      },
+      minW: 40,
+      p: 2,
+      ps: 6,
+      position: 'absolute',
+      right: 8,
+      top: '35%',
+      transition: 'all 0.3s',
     });
   };
 
   return (
-    <div className={`${cExpSidebar_sidebarItem} ${cExpSidebar_sidebarItemActive(index, currentActive)}`}>
-      <h2>{exp.company}</h2>
+    <div className={`group ${cExpSidebarItem_item} ${cExpSidebarItem_activeState(index, currentActive)}`}>
+      <h2 className={cExpSidebarItem_title(index, currentActive)}>{exp.company}</h2>
     </div>
   );
 }
