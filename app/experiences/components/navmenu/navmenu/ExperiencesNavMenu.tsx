e5997b1ec8afde96c1372de5d4ea6a335c4af304
Timeuh <timeuuh@gmail.com>
2024-04-10 13:43:32 +0200

    :sparkles: use nav menu menu component

--- app/experiences/components/navmenu/navmenu/ExperiencesNavMenu.tsx
+++ app/experiences/components/navmenu/navmenu/ExperiencesNavMenu.tsx
@@ -1,9 +1,11 @@
 import ExperiencesNavMenuButton from '../button/ExperiencesNavMenuButton';
 import cExpNavMenu_container from './ExperiencesNavMenu.styles';
+import ExperiencesNavMenuMenu from '../menu/ExperiencesNavMenuMenu';
 
 export default function ExperiencesNavMenu() {
   return (
     <div className={cExpNavMenu_container}>
+      <ExperiencesNavMenuMenu />
       <ExperiencesNavMenuButton />
     </div>
   );
