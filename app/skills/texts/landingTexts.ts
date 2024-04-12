e878f332cabc02e99f9a7fa102c9c8de3b2ec386
Timeuh <timeuuh@gmail.com>
2024-04-12 16:47:27 +0200

    :speech_balloon: create texts for skills page landing section

---
+++ app/skills/texts/landingTexts.ts
@@ -0,0 +1,20 @@
+import {BaseLandingTexts} from '@appTypes/portfolio';
+
+const skillsLandingTexts: BaseLandingTexts = {
+  title: {
+    english: 'My skills',
+    french: 'Mes compétences',
+  },
+  explanations: {
+    english:
+      'Through my studies and professional experiences, I have been able to learn various technologies that are very useful to me for developing websites',
+    french:
+      'A travers mes études et mes expériences professionnelles, j’ai peu apprendre diverses technologies qui me sont très utiles pour développer des sites web',
+  },
+  link: {
+    english: 'Discover',
+    french: 'Découvrir',
+  },
+};
+
+export default skillsLandingTexts;
