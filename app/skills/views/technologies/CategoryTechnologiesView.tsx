2fab091e0f8cfe1c5eb3aafe934a5c7570bea7f5
Timeuh <timeuuh@gmail.com>
2024-04-13 15:40:48 +0200

    :sparkles: use category's technologies display component + adjust section's style

--- app/skills/views/technologies/CategoryTechnologiesView.tsx
+++ app/skills/views/technologies/CategoryTechnologiesView.tsx
@@ -3,23 +3,47 @@
 import useTechWithCategory from '@app/skills/hooks/useTechWithCategory';
 import {TechCategory} from '@appTypes/portfolio';
 import useLanguage from '@hooks/useLanguage';
+import TechnologiesDisplay from '../../components/technologies_section/TechnologiesDisplay';
+import {vstack} from '@/styled-system/patterns';
 
 type Props = {
   category: TechCategory;
+  reversed?: boolean;
 };
 
-export default function CategoryTechnologiesView({category}: Props) {
+export default function CategoryTechnologiesView({category, reversed = false}: Props) {
   const {filteredTechnologies, technologies} = useTechWithCategory(category);
   const {language} = useLanguage();
 
   const readyToDisplay = technologies.isSuccess && filteredTechnologies.length !== 0;
 
+  const vCatTechView_section = vstack({
+    bgGradient: 'to-b',
+    color: {
+      base: 'neutral.dark',
+      _dark: 'neutral.light',
+    },
+    gap: 4,
+    gradientFrom: {
+      base: reversed ? 'p_blue.light' : 'p_purple.light',
+      _dark: reversed ? 'p_blue.dark' : 'p_purple.dark',
+    },
+    gradientTo: {
+      base: reversed ? 'p_purple.light' : 'p_blue.light',
+      _dark: reversed ? 'p_purple.dark' : 'p_blue.dark',
+    },
+    h: 'fit',
+    justifyContent: 'center',
+    minH: 'screen',
+    p: 4,
+    pb: 8,
+    w: 'full',
+  });
+
   return (
-    <section id={`skills-${TechCategory[category].toLowerCase()}`}>
+    <section id={`skills-${TechCategory[category].toLowerCase()}`} className={vCatTechView_section}>
       {readyToDisplay ? (
-        <>
-          <h1>{filteredTechnologies[0].category.name[language]}</h1>
-        </>
+        <TechnologiesDisplay technologies={filteredTechnologies} language={language} />
       ) : (
         <h1>loading</h1>
       )}
