72f005df8e5792d59a0729fb896f375425544e3a
Timeuh <timeuuh@gmail.com>
2024-04-13 14:12:46 +0200

    :recycle: use a local state to store filtered technologies and return them

--- app/skills/hooks/useTechWithCategory.tsx
+++ app/skills/hooks/useTechWithCategory.tsx
@@ -1,28 +1,27 @@
-import {TechCategory} from '@appTypes/portfolio';
+import {TechCategory, TechCategoryUtils} from '@appTypes/portfolio';
 import useTechnologies from '@app/skills/hooks/useTechnologies';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import {CompleteTechnologyFromApi} from '@schemas/api/technology/technology.schema';
-import {UseQueryResult} from '@tanstack/react-query';
-import {ApiCollection} from '@appTypes/api';
 
 /**
  * Custom hook to get the technologies from a specified category
  *
  * @param category The category of the technologies to fetch
  */
-const useTechWithCategory = (category: TechCategory): UseQueryResult<ApiCollection<CompleteTechnologyFromApi>> => {
+const useTechWithCategory = (category: TechCategory): TechCategoryUtils => {
   const technologies = useTechnologies();
+  const [filteredTechnologies, setFilteredTechnologies] = useState<CompleteTechnologyFromApi[]>([]);
 
   useEffect(() => {
     if (technologies.isSuccess) {
-      technologies.data.items = technologies.data.items.filter((tech: CompleteTechnologyFromApi) => {
+      const filteredTechs = technologies.data.items.filter((tech: CompleteTechnologyFromApi) => {
         return tech.category.id === category;
       });
-      technologies.data.count = technologies.data.items.length;
+      setFilteredTechnologies(filteredTechs);
     }
-  }, [technologies, category]);
+  }, [technologies.data?.items, technologies.isSuccess, category]);
 
-  return technologies;
+  return {filteredTechnologies, technologies};
 };
 
 export default useTechWithCategory;
