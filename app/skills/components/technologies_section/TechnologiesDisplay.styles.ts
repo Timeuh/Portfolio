1a6eca1c4aab13acc5b656c28d144dd0c9f8e010
Timeuh <timeuuh@gmail.com>
2024-04-13 20:37:23 +0200

    :recycle: move styles into a dedica

---
+++ app/skills/components/technologies_section/TechnologiesDisplay.styles.ts
@@ -0,0 +1,68 @@
+import {grid, vstack} from '@/styled-system/patterns';
+import {css} from '@/styled-system/css';
+import {TechCategory} from '@appTypes/portfolio';
+
+const cTechDisplay_container = vstack({
+  gap: 8,
+  justifyContent: 'center',
+});
+
+const cTechDisplay_headerContainer = vstack({
+  justifyContent: 'center',
+});
+
+const cTechDisplay_headerDecorator = css({
+  bg: 'p_blue.main',
+  h: 1,
+  w: '40vw',
+});
+
+const cTechDisplay_headerDecoratorColor = (category: TechCategory, direction: 'left' | 'right') => {
+  switch (category) {
+    case TechCategory.MASTERED:
+      return css({
+        bg: 'p_blue.main',
+      });
+
+    case TechCategory.LEARNING:
+      return css({
+        bgGradient: direction === 'right' ? 'to-r' : 'to-l',
+        gradientFrom: 'p_blue.main',
+        gradientTo: 'p_purple.main',
+      });
+
+    case TechCategory.PLANNED:
+      return css({
+        bgGradient: direction === 'right' ? 'to-r' : 'to-l',
+        gradientFrom: 'p_purple.main',
+        gradientTo: 'p_blue.main',
+      });
+
+    default:
+      return css({
+        bg: 'p_purple.main',
+      });
+  }
+};
+
+const cTechDisplay_headerTitle = css({
+  fontFamily: 'bricolage',
+  fontSize: '3xl',
+  fontWeight: 'bold',
+  lineHeight: 1,
+  textAlign: 'center',
+});
+
+const cTechDisplay_technologiesContainer = grid({
+  gap: 4,
+  gridTemplateColumns: 2,
+});
+
+export {
+  cTechDisplay_technologiesContainer,
+  cTechDisplay_headerTitle,
+  cTechDisplay_headerDecoratorColor,
+  cTechDisplay_headerDecorator,
+  cTechDisplay_headerContainer,
+  cTechDisplay_container,
+};
--- app/skills/components/technologies_section/TechnologiesDisplay.tsx
+++ app/skills/components/technologies_section/TechnologiesDisplay.tsx
@@ -1,8 +1,14 @@
 import {CompleteTechnologyFromApi} from '@schemas/api/technology/technology.schema';
 import {Language, TechCategory} from '@appTypes/portfolio';
-import {grid, vstack} from '@/styled-system/patterns';
-import {css} from '@/styled-system/css';
 import Technology from '@app/skills/components/technology/Technology';
+import {
+  cTechDisplay_container,
+  cTechDisplay_headerContainer,
+  cTechDisplay_headerDecorator,
+  cTechDisplay_headerDecoratorColor,
+  cTechDisplay_headerTitle,
+  cTechDisplay_technologiesContainer,
+} from './TechnologiesDisplay.styles';
 
 type Props = {
   technologies: CompleteTechnologyFromApi[];
@@ -11,62 +17,6 @@ type Props = {
 };
 
 export default function TechnologiesDisplay({technologies, language, category}: Props) {
-  const cTechDisplay_container = vstack({
-    gap: 8,
-    justifyContent: 'center',
-  });
-
-  const cTechDisplay_headerContainer = vstack({
-    justifyContent: 'center',
-  });
-
-  const cTechDisplay_headerDecorator = css({
-    bg: 'p_blue.main',
-    h: 1,
-    w: '40vw',
-  });
-
-  const cTechDisplay_headerDecoratorColor = (category: TechCategory, direction: 'left' | 'right') => {
-    switch (category) {
-      case TechCategory.MASTERED:
-        return css({
-          bg: 'p_blue.main',
-        });
-
-      case TechCategory.LEARNING:
-        return css({
-          bgGradient: direction === 'right' ? 'to-r' : 'to-l',
-          gradientFrom: 'p_blue.main',
-          gradientTo: 'p_purple.main',
-        });
-
-      case TechCategory.PLANNED:
-        return css({
-          bgGradient: direction === 'right' ? 'to-r' : 'to-l',
-          gradientFrom: 'p_purple.main',
-          gradientTo: 'p_blue.main',
-        });
-
-      default:
-        return css({
-          bg: 'p_purple.main',
-        });
-    }
-  };
-
-  const cTechDisplay_headerTitle = css({
-    fontFamily: 'bricolage',
-    fontSize: '3xl',
-    fontWeight: 'bold',
-    lineHeight: 1,
-    textAlign: 'center',
-  });
-
-  const cTechDisplay_technologiesContainer = grid({
-    gap: 4,
-    gridTemplateColumns: 2,
-  });
-
   return (
     <div className={cTechDisplay_container}>
       <div className={cTechDisplay_headerContainer}>
