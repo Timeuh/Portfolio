d2fb8725ee80ed86692dc9be14e268937fbdd0a4
Timeuh <timeuuh@gmail.com>
2024-04-13 11:25:13 +0200

    :sparkles: use views for each tech categories

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,9 +1,15 @@
 import SkillsLandingView from './views/landing/SkillsLandingView';
+import TechCategoryView from './views/technologies/TechCategoryView';
+import {TechCategory} from '@appTypes/portfolio';
 
 export default function Page() {
   return (
     <main>
       <SkillsLandingView />
+      <TechCategoryView category={TechCategory.TOOLS} />
+      <TechCategoryView category={TechCategory.MASTERED} />
+      <TechCategoryView category={TechCategory.LEARNING} />
+      <TechCategoryView category={TechCategory.PLANNED} />
     </main>
   );
 }
