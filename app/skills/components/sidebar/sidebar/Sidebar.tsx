80d41c21fc10b8551f4b6f5ad8beb0338f93d8fc
Timeuh <timeuuh@gmail.com>
2024-04-18 18:09:42 +0200

    :sparkles: fill sidebar content

--- app/skills/components/sidebar/sidebar/Sidebar.tsx
+++ app/skills/components/sidebar/sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import {vstack} from '@/styled-system/patterns';
+import {circle, vstack} from '@/styled-system/patterns';
 import {css} from '@/styled-system/css';
 
 export default function Sidebar() {
@@ -10,27 +10,142 @@ export default function Sidebar() {
       base: 'none',
       xl: 'flex',
     },
-    gap: 4,
+    gap: 14,
     position: 'sticky',
     top: 0,
     w: 'full',
     mt: '-100vh',
     h: 'screen',
-    ps: 8,
+    pt: 16,
+    ps: 20,
   });
 
-  const cSkillSidebar_item = css({
-    bg: 'p_blue.main',
-    h: 20,
+  const cSkillSidebar_item = vstack({
+    bg: {
+      base: 'neutral.light',
+      _dark: 'neutral.700',
+    },
+    borderRadius: 'full',
+    h: 32,
+    justifyContent: 'center',
+    shadow: {
+      base: '0 2px 4px 0 token(colors.p_blue.dark)',
+      _dark: '0 2px 4px 0 token(colors.p_blue.light)',
+    },
     w: 4,
+    transition: 'all 0.5s',
+  });
+
+  const cSkillSidebar_circle = circle({
+    bg: {
+      base: 'neutral.light',
+      _dark: 'neutral.700',
+    },
+    shadow: {
+      base: '0 2px 4px 0 token(colors.p_blue.dark)',
+      _dark: '0 2px 4px 0 token(colors.p_blue.light)',
+    },
+    size: 16,
+    transition: 'all 0.5s',
+  });
+
+  const cSkillSidebar_svg = css({
+    h: 10,
+    stroke: {
+      base: 'neutral.700',
+      _dark: 'neutral.light',
+    },
+    strokeWidth: 1.5,
+    w: 10,
   });
 
   return (
     <div className={cSkillSidebar_container}>
-      <div className={cSkillSidebar_item} />
-      <div className={cSkillSidebar_item} />
-      <div className={cSkillSidebar_item} />
-      <div className={cSkillSidebar_item} />
+      <div className={cSkillSidebar_item}>
+        <div className={cSkillSidebar_circle}>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            className={cSkillSidebar_svg}
+          >
+            <path d='m15 12-8.373 8.373a1 1 0 1 1-3-3L12 9' />
+            <path d='m18 15 4-4' />
+            <path d='m21.5 11.5-1.914-1.914A2 2 0 0 1 19 8.172V7l-2.26-2.26a6 6 0 0 0-4.202-1.756L9 2.96l.92.82A6.18 6.18 0 0 1 12 8.4V10l2 2h1.172a2 2 0 0 1 1.414.586L18.5 14.5' />
+          </svg>
+        </div>
+      </div>
+      <div className={cSkillSidebar_item}>
+        <div className={cSkillSidebar_circle}>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            className={cSkillSidebar_svg}
+          >
+            <path d='M21.42 10.922a1 1 0 0 0-.019-1.838L12.83 5.18a2 2 0 0 0-1.66 0L2.6 9.08a1 1 0 0 0 0 1.832l8.57 3.908a2 2 0 0 0 1.66 0z' />
+            <path d='M22 10v6' />
+            <path d='M6 12.5V16a6 3 0 0 0 12 0v-3.5' />
+          </svg>
+        </div>
+      </div>
+      <div className={cSkillSidebar_item}>
+        <div className={cSkillSidebar_circle}>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            className={cSkillSidebar_svg}
+          >
+            <path d='M13.4 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-7.4' />
+            <path d='M2 6h4' />
+            <path d='M2 10h4' />
+            <path d='M2 14h4' />
+            <path d='M2 18h4' />
+            <path d='M18.4 2.6a2.17 2.17 0 0 1 3 3L16 11l-4 1 1-4Z' />
+          </svg>
+        </div>
+      </div>
+      <div className={cSkillSidebar_item}>
+        <div className={cSkillSidebar_circle}>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            className={cSkillSidebar_svg}
+          >
+            <path d='M2 6h4' />
+            <path d='M2 10h4' />
+            <path d='M2 14h4' />
+            <path d='M2 18h4' />
+            <rect width='16' height='20' x='4' y='2' rx='2' />
+            <path d='M15 2v20' />
+            <path d='M15 7h5' />
+            <path d='M15 12h5' />
+            <path d='M15 17h5' />
+          </svg>
+        </div>
+      </div>
     </div>
   );
 }
