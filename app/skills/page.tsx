200a05c0c2e802010942308879349571f6f47ab3
Timeuh <timeuuh@gmail.com>
2024-04-18 17:39:07 +0200

    :sparkles: use skills sidebar component

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,12 +1,14 @@
 import SkillsLandingView from './views/landing/SkillsLandingView';
 import CategoryTechnologiesView from './views/technologies/CategoryTechnologiesView';
 import {TechCategory} from '@appTypes/portfolio';
+import Sidebar from '@app/skills/components/sidebar/sidebar/Sidebar';
 
 export default function Page() {
   return (
     <main>
       <SkillsLandingView />
       <section id={'skills-display'}>
+        <Sidebar />
         <CategoryTechnologiesView category={TechCategory.TOOLS} />
         <CategoryTechnologiesView category={TechCategory.MASTERED} reversed />
         <CategoryTechnologiesView category={TechCategory.LEARNING} />
