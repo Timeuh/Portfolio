898bd31e9ace408ae5c17bcaee52fa6eb66770e1
Timeuh <timeuuh@gmail.com>
2024-04-18 21:38:03 +0200

    :sparkles: use mobile navmenu component

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,7 +1,8 @@
 import SkillsLandingView from './views/landing/SkillsLandingView';
 import CategoryTechnologiesView from './views/technologies/CategoryTechnologiesView';
 import {TechCategory} from '@appTypes/portfolio';
-import Sidebar from '@app/skills/components/sidebar/sidebar/Sidebar';
+import Sidebar from './components/sidebar/sidebar/Sidebar';
+import NavMenu from './components/navmenu/navmenu/NavMenu';
 
 export default function Page() {
   return (
@@ -9,6 +10,7 @@ export default function Page() {
       <SkillsLandingView />
       <section id={'skills-display'}>
         <Sidebar />
+        <NavMenu />
         <CategoryTechnologiesView category={TechCategory.TOOLS} />
         <CategoryTechnologiesView category={TechCategory.MASTERED} reversed />
         <CategoryTechnologiesView category={TechCategory.LEARNING} />
