0070e49984ba7be96f24230f6f21bed6bd56d5c6
Timeuh <timeuuh@gmail.com>
2024-04-18 22:03:19 +0200

    :recycle: move navmenu item logic into a dedicated component

---
+++ app/skills/components/navmenu/menu_item/NavMenuItem.styles.ts
@@ -0,0 +1,65 @@
+import {hstack, vstack} from '@/styled-system/patterns';
+import {css} from '@/styled-system/css';
+
+const cSkillNavMenu_item = (index: number, currentActive: number) => {
+  const isActive = index === currentActive;
+
+  return hstack({
+    bg: {
+      base: isActive ? 'p_purple.700' : 'neutral.light',
+      _dark: isActive ? 'p_purple.200' : 'neutral.700',
+    },
+    borderBottom: index !== 3 ? '2px solid' : 0,
+    borderColor: {
+      base: 'p_purple.dark',
+      _dark: 'p_purple.light',
+    },
+    borderBottomRightRadius: index === 3 ? 'lg' : 0,
+    borderTopRightRadius: index === 0 ? 'lg' : 0,
+    color: {
+      base: isActive ? 'neutral.light' : 'neutral.700',
+      _dark: isActive ? 'neutral.dark' : 'neutral.light',
+    },
+    gap: 0,
+    h: '15vh',
+    justifyContent: 'center',
+    position: 'relative',
+    transition: 'all 0.3s',
+    w: '55vw',
+  });
+};
+
+const cSkillNavMenu_textContainer = vstack({
+  gap: 2,
+  justifyContent: 'center',
+  p: 2,
+  ps: 4,
+  transition: 'all 0.3s',
+});
+
+const cSkillNavMenu_text = css({
+  fontSize: 'sm',
+  textAlign: 'center',
+});
+
+const cSkillNavMenu_step = (index: number, currentActive: number) => {
+  const isActive = index === currentActive;
+
+  return css({
+    bg: {
+      base: isActive ? 'p_purple.700' : 'neutral.light',
+      _dark: isActive ? 'p_purple.200' : 'neutral.700',
+    },
+    h: 'full',
+    left: 0,
+    position: 'absolute',
+    shadow: {
+      base: isActive ? '0 2px 4px 2px token(colors.p_purple.light)' : '0 2px 4px 2px token(colors.p_blue.dark)',
+      _dark: isActive ? '0 2px 4px 2px token(colors.p_purple.dark)' : '0 2px 4px 2px token(colors.p_blue.light)',
+    },
+    transition: 'all 0.3s',
+    w: 4,
+  });
+};
+
+export {cSkillNavMenu_text, cSkillNavMenu_step, cSkillNavMenu_textContainer, cSkillNavMenu_item};
--- app/skills/components/navmenu/navmenu/NavMenu.tsx
+++ app/skills/components/navmenu/navmenu/NavMenu.tsx
@@ -1,20 +1,11 @@
 'use client';
 
-import sidebarLinks from '@app/skills/texts/sidebarLinks';
 import useScrollStep from '@app/skills/hooks/useScrollStep';
 import useLanguage from '@hooks/useLanguage';
-import Link from 'next/link';
-import {
-  cSkillNavMenu_container,
-  cSkillNavMenu_item,
-  cSkillNavMenu_menu,
-  cSkillNavMenu_step,
-  cSkillNavMenu_svg,
-  cSkillNavMenu_text,
-  cSkillNavMenu_textContainer,
-} from './NavMenu.styles';
+import {cSkillNavMenu_container, cSkillNavMenu_menu, cSkillNavMenu_svg} from './NavMenu.styles';
 import NavMenuButton from '../button/NavMenuButton';
 import {useState} from 'react';
+import NavMenuItem from '../menu_item/NavMenuItem';
 
 export default function NavMenu() {
   const currentActive = useScrollStep();
@@ -28,99 +19,83 @@ export default function NavMenu() {
   return (
     <div className={cSkillNavMenu_container}>
       <aside className={cSkillNavMenu_menu(showMenu)}>
-        <Link href={sidebarLinks.links[0]} className={cSkillNavMenu_item(0, currentActive)} onClick={toggleMenu}>
-          <div className={cSkillNavMenu_step(0, currentActive)} />
-          <div className={cSkillNavMenu_textContainer}>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='24'
-              height='24'
-              viewBox='0 0 24 24'
-              fill='none'
-              stroke='currentColor'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              className={cSkillNavMenu_svg(0, currentActive)}
-            >
-              <path d='m15 12-8.373 8.373a1 1 0 1 1-3-3L12 9' />
-              <path d='m18 15 4-4' />
-              <path d='m21.5 11.5-1.914-1.914A2 2 0 0 1 19 8.172V7l-2.26-2.26a6 6 0 0 0-4.202-1.756L9 2.96l.92.82A6.18 6.18 0 0 1 12 8.4V10l2 2h1.172a2 2 0 0 1 1.414.586L18.5 14.5' />
-            </svg>
-            <h2 className={cSkillNavMenu_text}>{sidebarLinks.titles[0][language]}</h2>
-          </div>
-        </Link>
-        <Link href={sidebarLinks.links[1]} className={cSkillNavMenu_item(1, currentActive)} onClick={toggleMenu}>
-          <div className={cSkillNavMenu_step(1, currentActive)} />
-          <div className={cSkillNavMenu_textContainer}>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='24'
-              height='24'
-              viewBox='0 0 24 24'
-              fill='none'
-              stroke='currentColor'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              className={cSkillNavMenu_svg(1, currentActive)}
-            >
-              <path d='M21.42 10.922a1 1 0 0 0-.019-1.838L12.83 5.18a2 2 0 0 0-1.66 0L2.6 9.08a1 1 0 0 0 0 1.832l8.57 3.908a2 2 0 0 0 1.66 0z' />
-              <path d='M22 10v6' />
-              <path d='M6 12.5V16a6 3 0 0 0 12 0v-3.5' />
-            </svg>
-            <h2 className={cSkillNavMenu_text}>{sidebarLinks.titles[1][language]}</h2>
-          </div>
-        </Link>
-        <Link href={sidebarLinks.links[2]} className={cSkillNavMenu_item(2, currentActive)} onClick={toggleMenu}>
-          <div className={cSkillNavMenu_step(2, currentActive)} />
-          <div className={cSkillNavMenu_textContainer}>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='24'
-              height='24'
-              viewBox='0 0 24 24'
-              fill='none'
-              stroke='currentColor'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              className={cSkillNavMenu_svg(2, currentActive)}
-            >
-              <path d='M13.4 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-7.4' />
-              <path d='M2 6h4' />
-              <path d='M2 10h4' />
-              <path d='M2 14h4' />
-              <path d='M2 18h4' />
-              <path d='M18.4 2.6a2.17 2.17 0 0 1 3 3L16 11l-4 1 1-4Z' />
-            </svg>
-            <h2 className={cSkillNavMenu_text}>{sidebarLinks.titles[2][language]}</h2>
-          </div>
-        </Link>
-        <Link href={sidebarLinks.links[3]} className={cSkillNavMenu_item(3, currentActive)} onClick={toggleMenu}>
-          <div className={cSkillNavMenu_step(3, currentActive)} />
-          <div className={cSkillNavMenu_textContainer}>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='24'
-              height='24'
-              viewBox='0 0 24 24'
-              fill='none'
-              stroke='currentColor'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              className={cSkillNavMenu_svg(3, currentActive)}
-            >
-              <path d='M2 6h4' />
-              <path d='M2 10h4' />
-              <path d='M2 14h4' />
-              <path d='M2 18h4' />
-              <rect width='16' height='20' x='4' y='2' rx='2' />
-              <path d='M15 2v20' />
-              <path d='M15 7h5' />
-              <path d='M15 12h5' />
-              <path d='M15 17h5' />
-            </svg>
-            <h2 className={cSkillNavMenu_text}>{sidebarLinks.titles[3][language]}</h2>
-          </div>
-        </Link>
+        <NavMenuItem toggleMenu={toggleMenu} index={0} currentActive={currentActive} language={language}>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            className={cSkillNavMenu_svg(0, currentActive)}
+          >
+            <path d='m15 12-8.373 8.373a1 1 0 1 1-3-3L12 9' />
+            <path d='m18 15 4-4' />
+            <path d='m21.5 11.5-1.914-1.914A2 2 0 0 1 19 8.172V7l-2.26-2.26a6 6 0 0 0-4.202-1.756L9 2.96l.92.82A6.18 6.18 0 0 1 12 8.4V10l2 2h1.172a2 2 0 0 1 1.414.586L18.5 14.5' />
+          </svg>
+        </NavMenuItem>
+        <NavMenuItem toggleMenu={toggleMenu} index={1} currentActive={currentActive} language={language}>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            className={cSkillNavMenu_svg(1, currentActive)}
+          >
+            <path d='M21.42 10.922a1 1 0 0 0-.019-1.838L12.83 5.18a2 2 0 0 0-1.66 0L2.6 9.08a1 1 0 0 0 0 1.832l8.57 3.908a2 2 0 0 0 1.66 0z' />
+            <path d='M22 10v6' />
+            <path d='M6 12.5V16a6 3 0 0 0 12 0v-3.5' />
+          </svg>
+        </NavMenuItem>
+        <NavMenuItem toggleMenu={toggleMenu} index={2} currentActive={currentActive} language={language}>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            className={cSkillNavMenu_svg(2, currentActive)}
+          >
+            <path d='M13.4 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-7.4' />
+            <path d='M2 6h4' />
+            <path d='M2 10h4' />
+            <path d='M2 14h4' />
+            <path d='M2 18h4' />
+            <path d='M18.4 2.6a2.17 2.17 0 0 1 3 3L16 11l-4 1 1-4Z' />
+          </svg>
+        </NavMenuItem>
+        <NavMenuItem toggleMenu={toggleMenu} index={3} currentActive={currentActive} language={language}>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            className={cSkillNavMenu_svg(3, currentActive)}
+          >
+            <path d='M2 6h4' />
+            <path d='M2 10h4' />
+            <path d='M2 14h4' />
+            <path d='M2 18h4' />
+            <rect width='16' height='20' x='4' y='2' rx='2' />
+            <path d='M15 2v20' />
+            <path d='M15 7h5' />
+            <path d='M15 12h5' />
+            <path d='M15 17h5' />
+          </svg>
+        </NavMenuItem>
       </aside>
       <NavMenuButton toggleMenu={toggleMenu} />
     </div>
