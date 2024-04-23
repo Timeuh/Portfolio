bfc7a7c32935e530422809abbb8de16226e23964
Timeuh <timeuuh@gmail.com>
2024-04-23 21:52:29 +0200

    :sparkles: create custom hook to fetch projects

---
+++ app/projects/hooks/useProjects.ts
@@ -0,0 +1,22 @@
+import {useQuery, UseQueryResult} from '@tanstack/react-query';
+import {ApiCollection} from '@appTypes/api';
+import {CompleteProjectFromApi} from '@schemas/api/project/project.schema';
+
+/**
+ * Fetches the projects from the API
+ */
+const getProjects = async (): Promise<ApiCollection<CompleteProjectFromApi>> => {
+  return await fetch('/api/projects?fullContent=true').then((res: Response) => {
+    return res.json();
+  });
+};
+
+/**
+ * Custom hook to fetch the projects from the API with react-query
+ */
+const useProjects = (): UseQueryResult<ApiCollection<CompleteProjectFromApi>> => {
+  return useQuery<ApiCollection<CompleteProjectFromApi>>({
+    queryKey: ['projects'],
+    queryFn: getProjects,
+  });
+};
