c6f9bdb53f2d60240cc4e6659b283665a5f1c6f7
Timeuh <timeuuh@gmail.com>
2024-04-23 20:15:46 +0200

    :sparkles: add page metadata

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,4 +1,10 @@
 import ProjectsLandingView from '@app/projects/views/ProjectsLanding/ProjectsLandingView';
+import {Metadata} from 'next';
+
+export const metadata: Metadata = {
+  title: 'Timeuh - Projets',
+  description: 'Page presenting Timeuh projects.',
+};
 
 export default function Page() {
   return (
