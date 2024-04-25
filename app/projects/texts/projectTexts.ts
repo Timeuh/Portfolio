02121534aad48e26d2baa46b829801820d1d115d
Timeuh <timeuuh@gmail.com>
2024-04-25 21:01:32 +0200

    :speech_balloon: create project component texts

---
+++ app/projects/texts/projectTexts.ts
@@ -0,0 +1,18 @@
+import {ProjectTexts} from '@appTypes/portfolio';
+
+const projectTexts: ProjectTexts = {
+  techsTitle: {
+    english: 'Technologies used',
+    french: 'Les technologies utilisées',
+  },
+  consultTitle: {
+    english: 'Consult the project',
+    french: 'Consulter le projet',
+  },
+  websiteTitle: {
+    english: 'Website',
+    french: 'Site web',
+  },
+};
+
+export default projectTexts;
