7e70e8808e7be3b6d93d77f94a93fdc9e5844080
Timeuh <timeuuh@gmail.com>
2024-04-25 21:52:07 +0200

    :lipstick: add styles for project index 0

--- app/projects/components/project_decoration/ProjectDecoration.styles.ts
+++ app/projects/components/project_decoration/ProjectDecoration.styles.ts
@@ -5,11 +5,39 @@ const cProjDecoration_decoration = css({
     base: 'neutral.300',
     _dark: 'neutral.700',
   },
+  borderBottomLeftRadius: 'xl',
   position: 'absolute',
   top: 0,
-  h: 'full',
+  h: '98%',
   transition: 'all 0.5s',
-  w: 10,
+  w: 8,
 });
 
-export {cProjDecoration_decoration};
+const cProjDecoration_decorationTwo = css({
+  bg: {
+    base: 'neutral.300',
+    _dark: 'neutral.700',
+  },
+  borderBottomLeftRadius: 'xl',
+  position: 'absolute',
+  bottom: 0,
+  h: 8,
+  transition: 'all 0.5s',
+  w: 32,
+});
+
+const cProjDecoration_decorationThree = css({
+  bg: {
+    base: 'neutral.300',
+    _dark: 'neutral.700',
+  },
+  borderTopRightRadius: 'xl',
+  position: 'absolute',
+  bottom: -6,
+  h: 14,
+  left: 32,
+  transition: 'all 0.5s',
+  w: 8,
+});
+
+export {cProjDecoration_decoration, cProjDecoration_decorationTwo, cProjDecoration_decorationThree};
