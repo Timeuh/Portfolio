d92bf689a5d1d0163f34934e76dc72528ce679bd
Timeuh <timeuuh@gmail.com>
2024-04-25 21:51:50 +0200

    :sparkles: return decoration based on index (WIP)

--- app/projects/components/project_decoration/ProjectDecoration.tsx
+++ app/projects/components/project_decoration/ProjectDecoration.tsx
@@ -1,9 +1,33 @@
-import {cProjDecoration_decoration} from './ProjectDecoration.styles';
+import {
+  cProjDecoration_decoration,
+  cProjDecoration_decorationThree,
+  cProjDecoration_decorationTwo,
+} from './ProjectDecoration.styles';
 
 type Props = {
   index: number;
 };
 
 export default function ProjectDecoration({index}: Props) {
-  return <div className={cProjDecoration_decoration} />;
+  switch (index) {
+    case 0:
+      return (
+        <div className={cProjDecoration_decoration}>
+          <div className={cProjDecoration_decorationTwo} />
+          <div className={cProjDecoration_decorationThree} />
+        </div>
+      );
+
+    case 1:
+      return <div className={cProjDecoration_decoration} />;
+
+    case 2:
+      return <div className={cProjDecoration_decoration} />;
+
+    case 3:
+      return <div className={cProjDecoration_decoration} />;
+
+    default:
+      return <div className={cProjDecoration_decoration} />;
+  }
 }
