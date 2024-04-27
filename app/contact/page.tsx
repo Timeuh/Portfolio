d4c6fdf669aa88ffbb585571a67a6ccfc2b54c5a
Timeuh <timeuuh@gmail.com>
2024-04-27 16:16:10 +0200

    :sparkles: create contact page (WIP)

---
+++ app/contact/page.tsx
@@ -0,0 +1,14 @@
+import {Metadata} from 'next';
+
+export const metadata: Metadata = {
+  title: 'Timeuh - Contact',
+  description: 'Different ways to contact Timeuh.',
+};
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Contact</h1>
+    </main>
+  );
+}
