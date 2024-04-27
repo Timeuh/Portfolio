8cbf046af517921594fb10779cb5a5f380c8d372
Timeuh <timeuuh@gmail.com>
2024-04-27 21:00:59 +0200

    :speech_balloon: create texts for legal page author section

---
+++ app/legal/texts/authorTexts.ts
@@ -0,0 +1,54 @@
+import {LegalAuthorTexts} from '@appTypes/portfolio';
+
+const authorTexts: LegalAuthorTexts = {
+  title: {
+    english: 'Copyright',
+    french: 'Droits D’auteur',
+  },
+  firstParagraph: {
+    english: 'This website is made by Timothée Brindejonc. It is subject to copyright law.',
+    french: 'Ce site est réalisé par Timothée Brindejonc. Il est soumis à la législation sur les droits d’auteur.',
+  },
+  secondParagraphFirstPart: {
+    english: 'The icons used on this website are the property of the website',
+    french: 'Les icônes utilisées sur ce cite sont la propriété du site',
+  },
+  secondParagraphSecondPart: {
+    english:
+      ', and are only lent for non-commercial use. Some illustration images were lent by the website Unsplash under the condition of crediting the authors:',
+    french:
+      ', et ne sont prêtées que pour un usage non commercial. Certaines images d’illustration ont été prêtées par le site Unsplash à condition de créditer les auteurs :',
+  },
+  farzadLink: {
+    english: 'Photo by Farzad on Unsplash',
+    french: 'Photo de Farzad sur Unsplash',
+  },
+  domFouLink: {
+    english: 'Photo by Dom Fou on Unsplash',
+    french: 'Photo de Dom Fou sur Unsplash',
+  },
+  brankoLink: {
+    english: 'Photo by Branko Stancevic on Unsplash',
+    french: 'Photo de Branko Stancevic sur Unsplash',
+  },
+  scottLink: {
+    english: 'Photo by Scott Graham on Unsplash',
+    french: 'Photo de Scott Graham sur Unsplash',
+  },
+  huntersLink: {
+    english: 'Photo by Hunters Race on Unsplash',
+    french: 'Photo de Hunters Race sur Unsplash',
+  },
+  miaBakerLink: {
+    english: 'Photo by Mia Baker on Unsplash',
+    french: 'Photo de Mia Baker sur Unsplash',
+  },
+  lastParagraph: {
+    english:
+      'Any reproduction, representation, use or modification of this website is prohibited without the prior authorization of the creator.',
+    french:
+      'Toute reproduction, représentation, utilisation ou modification de ce site est interdite sans avoir obtenu l’autorisation préalable du créateur.',
+  },
+};
+
+export default authorTexts;
