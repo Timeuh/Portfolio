b348ae4a6587f280c4d080727e65fdab5136c131
Timeuh <timeuuh@gmail.com>
2024-04-27 21:30:52 +0200

    :sparkles: create legal mentions view (WIP)

---
+++ app/legal/views/legal_mentions/LegalMentionsView.tsx
@@ -0,0 +1,110 @@
+export default function LegalMentionsView() {
+  return (
+    <section id={'legal-mentions'}>
+      <h1>Mentions légales</h1>
+      <section>
+        <h2>1. PRÉSENTATION DU SITE.</h2>
+        <p>
+          En vertu de l’article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l’économie numérique, il
+          est précisé aux utilisateurs du site timeuh.fr l’identité des différents intervenants dans le cadre de sa
+          réalisation et de son suivi :
+        </p>
+        <p>Propriétaire et créateur : Timothée Brindejonc – 07 50 93 70 78 – 265 rue du Saint Michel, 54200 Toul.</p>
+        <p>Hébergeur : OVHCloud - 2 rue Kellermann, 59100 Roubaix</p>
+      </section>
+      <section>
+        <h2>2. CONDITIONS GÉNÉRALES D’UTILISATION DU SITE ET DES SERVICES PROPOSÉS.</h2>
+        <p>
+          L’utilisation du site timeuh.fr implique l’acceptation pleine et entière des conditions générales
+          d’utilisation ci-après décrites.
+        </p>
+      </section>
+      <section>
+        <h2>3. DESCRIPTION DES SERVICES FOURNIS.</h2>
+        <p>
+          Le site timeuh.fr a pour objet de fournir une information concernant l’ensemble des activités de Timothée
+          Brindejonc et s’efforce de fournir sur le site timeuh.fr des informations aussi précises que possible.
+        </p>
+        <p>
+          Toutefois, il ne pourra être tenu responsable des omissions, des inexactitudes et des carences dans la mise à
+          jour, qu’elles soient de son fait ou du fait des tiers partenaires qui lui fournissent ces informations. Tous
+          les informations indiquées sur le site timeuh.fr sont données à titre indicatif, et sont susceptibles
+          d’évoluer.
+        </p>
+        <p>
+          Par ailleurs, les renseignements figurant sur le site timeuh.fr ne sont pas exhaustifs. Ils sont donnés sous
+          réserve de modifications ayant été apportées depuis leur mise en ligne.
+        </p>
+      </section>
+      <section>
+        <h2>4. LIMITATIONS CONTRACTUELLES SUR LES DONNÉES TECHNIQUES.</h2>
+        <p>
+          Le site utilise la technologie JavaScript. Le site Internet ne pourra être tenu responsable de dommages
+          matériels liés à l’utilisation du site. De plus, l’utilisateur du site s’engage à accéder au site en utilisant
+          un matériel récent, ne contenant pas de virus et avec un navigateur de dernière génération mis-à-jour.
+        </p>
+      </section>
+      <section>
+        <h2>5. PROPRIÉTÉ INTELLECTUELLE ET CONTREFAÇONS.</h2>
+        <p>
+          Timothée Brindejonc est propriétaire des droits de propriété intellectuelle ou détient les droits d’usage sur
+          tout le code de production du site.
+        </p>
+        <p>
+          Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments du
+          site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable de
+          Timothée Brindejonc.
+        </p>
+        <p>
+          Toute exploitation non autorisée du site ou de l’un des quelconque éléments qu’il contient sera considérée
+          comme constitutive d’une contrefaçon et poursuivie conformément aux dispositions des articles L.335-2 et
+          suivants du Code de Propriété Intellectuelle.
+        </p>
+      </section>
+      <section>
+        <h2>6. LIMITATIONS DE RESPONSABILITÉ.</h2>
+        <p>
+          Timothée Brindejonc ne pourra être tenu responsable des dommages directs et indirects causés au matériel de
+          l’utilisateur, lors de l’accès au site timeuh.fr, et résultant soit de l’utilisation d’un matériel ne
+          répondant pas aux spécifications indiquées au point 4, soit de l’apparition d’un bug ou d’une incompatibilité.
+        </p>
+        <p>
+          Timothée Brindejonc ne pourra également être tenu responsable des dommages indirects (tels par exemple qu’une
+          perte de marché ou perte d’une chance) consécutifs à l’utilisation du site timeuh.fr.
+        </p>
+      </section>
+      <section>
+        <h2>7. GESTION DES DONNÉES PERSONNELLES.</h2>
+        <p>
+          En France, les données personnelles sont notamment protégées par la loi n° 78-87 du 6 janvier 1978, la loi n°
+          2004-801 du 6 août 2004, l’article L. 226-13 du Code pénal et la Directive Européenne du 24 octobre 1995.
+        </p>
+        <p>
+          A l’occasion de l’utilisation du site timeuh.fr, aucune information personnelle de l’utilisateur du site
+          timeuh.fr n’est récoltée, publiée à l’insu de l’utilisateur, échangée, transférée, cédée ou vendue sur un
+          support quelconque à des tiers, et ce même dans le cas d’un contact par email.
+        </p>
+      </section>
+      <section>
+        <h2>8. LIENS HYPERTEXTES ET COOKIES.</h2>
+        <p>
+          Le site timeuh.fr contient un certain nombre de liens hypertextes vers d’autres sites, mis en place avec
+          l’autorisation de Timothée Brindejonc.
+        </p>
+        <p>
+          La navigation sur le site timeuh.fr est susceptible de provoquer l’installation de cookie(s) sur l’ordinateur
+          de l’utilisateur, afin d’assurer le bon fonctionnement du site. Aucune donnée personnelle n’est recueillie via
+          des cookies, ni par quelconque autre moyen.
+        </p>
+      </section>
+      <section>
+        <h2>9. DROIT APPLICABLE ET ATTRIBUTION DE JURIDICTION. </h2>
+        <p>
+          Tout litige en relation avec l’utilisation du site timeuh.fr est soumis au droit français. Loi n° 78-87 du 6
+          janvier 1978, notamment modifiée par la loi n° 2004-801 du 6 août 2004 relative à l’informatique, aux fichiers
+          et aux libertés. Loi n° 2004-575 du 21 juin 2004 pour la confiance dans l’économie numérique.
+        </p>
+      </section>
+    </section>
+  );
+}
