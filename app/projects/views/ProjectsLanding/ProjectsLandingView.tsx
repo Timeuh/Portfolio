29cbc572363dfe6436c9794bc1dec486293cfcff
Timeuh <timeuuh@gmail.com>
2024-04-27 17:49:27 +0200

    Revert ":sparkles: add landing section content"
    This reverts commit 9cadc7d936ccb51c04c40c456be0f036af5e12a5.

--- app/projects/views/ProjectsLanding/ProjectsLandingView.tsx
+++ app/projects/views/ProjectsLanding/ProjectsLandingView.tsx
@@ -1,52 +1,64 @@
+import {css} from '@/styled-system/css';
 import Image from 'next/image';
-import Link from 'next/link';
-import {
-  vProjLanding_container,
-  vProjLanding_decoration,
-  vProjLanding_image,
-  vProjLanding_landing,
-  vProjLanding_link,
-  vProjLanding_linkSvg,
-  vProjLanding_paragraph,
-  vProjLanding_textContainer,
-  vProjLanding_title,
-} from './ProjectsLandingView.styles';
 
 export default function ProjectsLandingView() {
+  const vProjLanding_landing = css({
+    bg: {
+      base: 'p_purple.light',
+      _dark: 'p_purple.dark',
+    },
+    color: {
+      base: 'neutral.dark',
+      _dark: 'neutral.light',
+    },
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 4,
+    h: 'screen',
+    w: 'full',
+    transition: 'all 0.5s',
+  });
+
+  const vProjLanding_image: string = css({
+    borderRadius: '2xl',
+    h: {
+      base: '250px',
+      md: '350px',
+      xl: '500px',
+    },
+    objectFit: 'cover',
+    shadow: {
+      base: '4px 4px 5px 2px token(colors.p_blue.dark_25)',
+      _dark: '4px 4px 5px 2px token(colors.p_blue.light_25)',
+    },
+    w: {
+      base: '80%',
+      md: '50%',
+      xl: '70%',
+    },
+  });
+
+  const vProjLanding_paragraph: string = css({
+    fontSize: '2xl',
+    textWrap: 'balance',
+    w: 'full',
+  });
+
   return (
     <section id={'projects-landing'} className={vProjLanding_landing}>
-      <h1 className={vProjLanding_title}>Mes projets</h1>
-      <div className={vProjLanding_container}>
-        <Image
-          src={'/images/portfolio/projects/projects.jpg'}
-          alt={'me'}
-          width={0}
-          height={0}
-          sizes={'100vw'}
-          className={vProjLanding_image}
-        />
-        <div className={vProjLanding_textContainer}>
-          <p className={vProjLanding_paragraph}>
-            A travers mes études et mes expériences professionnelles, j’ai peu apprendre diverses technologies qui me
-            sont très utiles pour développer des sites web
-          </p>
-          <Link href={'#project-1'} className={`group ${vProjLanding_link}`}>
-            <h2>Découvrir</h2>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 24 24'
-              fill='none'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              className={vProjLanding_linkSvg}
-            >
-              <path d='M12 5v14' />
-              <path d='m19 12-7 7-7-7' />
-            </svg>
-          </Link>
-          <div className={vProjLanding_decoration} />
-        </div>
-      </div>
+      <h1>Mes projets</h1>
+      <Image
+        src={'/images/portfolio/experiences/costume.jpg'}
+        alt={'me'}
+        width={0}
+        height={0}
+        sizes={'100vw'}
+        className={vProjLanding_image}
+      />
+      <p className={vProjLanding_paragraph}>
+        A travers mes études et mes expériences professionnelles, j’ai peu apprendre diverses technologies qui me sont
+        très utiles pour développer des sites web
+      </p>
     </section>
   );
 }
