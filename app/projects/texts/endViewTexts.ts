b975dc05c09d20de8c6b99f02ddfed243aba7d8f
Timeuh <timeuuh@gmail.com>
2024-04-27 11:45:28 +0200

    :speech_balloon: create project end section texts

---
+++ app/projects/texts/endViewTexts.ts
@@ -0,0 +1,16 @@
+import {ProjectEndTexts} from '@appTypes/portfolio';
+
+const endViewTexts: ProjectEndTexts = {
+  text: {
+    english:
+      'If you want to see more about my projects, you can visit my Github page, to check my code and possibly the sites related to my projects!',
+    french:
+      'Si vous avez envie d’en voir plus sur mes projets, vous pouvez visiter ma page Github, pour consulter mon code et éventuellement les sites liés à mes projets !',
+  },
+  link: {
+    english: 'See more',
+    french: 'En voir plus',
+  },
+};
+
+export default endViewTexts;
