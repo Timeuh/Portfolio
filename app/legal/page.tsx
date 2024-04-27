1c6da6706eb24e9477a3daa7c27c7c98e7c6388c
Timeuh <timeuuh@gmail.com>
2024-04-27 19:01:20 +0200

    :sparkles: create legal page (WIP)

---
+++ app/legal/page.tsx
@@ -0,0 +1,14 @@
+import {Metadata} from 'next';
+
+export const metadata: Metadata = {
+  title: 'Timeuh - Mentions légales',
+  description: 'Page presenting Timeuh legal information.',
+};
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Legal</h1>
+    </main>
+  );
+}
