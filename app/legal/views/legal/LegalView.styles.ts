50789a73ebaaf70ae9caba48b80b6e736bbe44f2
Timeuh <timeuuh@gmail.com>
2024-04-27 20:47:07 +0200

    :lipstick: create legal view styles file

---
+++ app/legal/views/legal/LegalView.styles.ts
@@ -0,0 +1,39 @@
+import {vstack} from '@/styled-system/patterns';
+import {css} from '@/styled-system/css';
+
+const vLegal_legal = vstack({
+  alignItems: 'start',
+  bg: {
+    base: 'p_purple.light',
+    _dark: 'p_purple.dark',
+  },
+  color: {
+    base: 'neutral.800',
+    _dark: 'neutral.light',
+  },
+  p: 4,
+  textWrap: 'balance',
+  transition: 'all 0.5s',
+});
+
+const vLegal_bigHeading = css({
+  color: {
+    base: 'neutral.dark',
+    _dark: 'neutral.400',
+  },
+  fontFamily: 'bricolage',
+  fontSize: '3xl',
+  fontWeight: 'bold',
+});
+
+const vLegal_firstSection = vstack({
+  alignItems: 'start',
+  gap: 4,
+  pt: 12,
+});
+
+const vLegal_link = css({
+  textDecoration: 'underline',
+});
+
+export {vLegal_legal, vLegal_bigHeading, vLegal_firstSection, vLegal_link};
