d739db77466eeea74eb7bf71453e31cd734fb46d
Timeuh <timeuuh@gmail.com>
2024-04-27 11:58:55 +0200

    :recycle: handle loading state more user friendly

--- app/projects/views/projects/ProjectsView.tsx
+++ app/projects/views/projects/ProjectsView.tsx
@@ -3,26 +3,28 @@
 import useProjects from '@app/projects/hooks/useProjects';
 import ProjectSection from '../../components/project_section/ProjectSection';
 import {CompleteProjectFromApi} from '@schemas/api/project/project.schema';
+import LoadingProjectSection from '@app/projects/components/loading_project_section/LoadingProjectSection';
 
 export default function ProjectsView() {
   const projects = useProjects();
-
-  if (!projects.isSuccess) {
-    return;
-  }
+  const fakeLoading: any[] = new Array(4).fill(null);
 
   return (
     <section id={'projects-display'}>
-      {projects.data.items.map((project: CompleteProjectFromApi, index: number) => {
-        return (
-          <ProjectSection
-            key={project.id}
-            project={project}
-            direction={index % 2 === 0 ? 'left' : 'right'}
-            index={index}
-          />
-        );
-      })}
+      {projects.isSuccess
+        ? projects.data.items.map((project: CompleteProjectFromApi, index: number) => {
+            return (
+              <ProjectSection
+                key={project.id}
+                project={project}
+                direction={index % 2 === 0 ? 'left' : 'right'}
+                index={index}
+              />
+            );
+          })
+        : fakeLoading.map((_, index: number) => {
+            return <LoadingProjectSection key={index} index={index} />;
+          })}
     </section>
   );
 }
