443ff2d0409e65c5e0a2dffdbeb897a978d3886b
Timeuh <timeuuh@gmail.com>
2024-04-27 18:52:55 +0200

    :speech_balloon: create texts for project landing page

---
+++ app/projects/texts/landingTexts.ts
@@ -0,0 +1,20 @@
+import {ProjectLandingTexts} from '@appTypes/portfolio';
+
+const landingTexts: ProjectLandingTexts = {
+  title: {
+    english: 'My projects',
+    french: 'Mes projets',
+  },
+  paragraph: {
+    english:
+      'Through my studies and professional experiences, I have been able to learn various technologies that are very useful to me for developing websites',
+    french:
+      'A travers mes études et mes expériences professionnelles, j’ai peu apprendre diverses technologies qui me sont très utiles pour développer des sites web',
+  },
+  link: {
+    english: 'Discover',
+    french: 'Découvrir',
+  },
+};
+
+export default landingTexts;
