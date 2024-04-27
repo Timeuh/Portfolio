a8d5efc731b62d6ecb71eba2d717d9f772808e01
Timeuh <timeuuh@gmail.com>
2024-04-27 21:01:20 +0200

    :sparkles: create content for author section

--- app/legal/views/legal/LegalView.tsx
+++ app/legal/views/legal/LegalView.tsx
@@ -1,23 +1,60 @@
+'use client';
+
 import Link from 'next/link';
+import {vLegal_bigHeading, vLegal_firstSection, vLegal_legal, vLegal_link} from './LegalView.styles';
+import useLanguage from '@hooks/useLanguage';
+import authorTexts from '@app/legal/texts/authorTexts';
 
 export default function LegalView() {
+  const {language} = useLanguage();
+
   return (
-    <>
-      <section id={'author'}>
-        <h1>Droits D&apos;auteur</h1>
+    <section className={vLegal_legal}>
+      <section id={'author'} className={vLegal_firstSection}>
+        <h1 className={vLegal_bigHeading}>{authorTexts.title[language]}</h1>
+        <p>{authorTexts.firstParagraph[language]}</p>
         <p>
-          Ce site est réalisé par Timothée Brindejonc. Il est soumis à la législation sur les droits d&apos;auteur. Les
-          icônes utilisées sur ce cite sont la propriété du site Lucide Icons, et ne sont prêtées que pour un usage non
-          commercial. Certaines images d’illustration ont été prêtées par le site Unsplash à condition de créditer les
-          auteurs :
+          {authorTexts.secondParagraphFirstPart[language]}{' '}
+          <Link href={'https://lucide.dev'} className={vLegal_link}>
+            Lucide Icons
+          </Link>
+          {authorTexts.secondParagraphSecondPart[language]}
         </p>
-        <Link href={''}>Photo de Farzad sur Unsplash</Link>
-        <Link href={''}>Photo de Dom Fou sur Unsplash</Link>
-        <p>
-          Toute reproduction, représentation, utilisation ou modification de ce site est interdite sans avoir obtenu
-          l’autorisation préalable du créateur.
-        </p>
+        <Link
+          href={'https://unsplash.com/fr/photos/two-black-computer-monitors-on-black-table-p-xSl33Wxyc'}
+          className={vLegal_link}
+        >
+          {authorTexts.farzadLink[language]}
+        </Link>
+        <Link
+          href={'https://unsplash.com/fr/photos/personnes-assises-sur-une-chaise-devant-un-ordinateur-YRMWVcdyhmI'}
+          className={vLegal_link}
+        >
+          {authorTexts.domFouLink[language]}
+        </Link>
+        <Link href={'https://unsplash.com/fr/photos/codes-de-programmation-GI1hwOGqGtE'} className={vLegal_link}>
+          {authorTexts.brankoLink[language]}
+        </Link>
+        <Link
+          href={'https://unsplash.com/fr/photos/personne-tenant-un-crayon-pres-dun-ordinateur-portable-5fNmWej4tAA'}
+          className={vLegal_link}
+        >
+          {authorTexts.scottLink[language]}
+        </Link>
+        <Link
+          href={'https://unsplash.com/fr/photos/personne-debout-pres-de-lescalier-MYbhN8KaaEc'}
+          className={vLegal_link}
+        >
+          {authorTexts.huntersLink[language]}
+        </Link>
+        <Link
+          href={'https://unsplash.com/fr/photos/personne-tenant-liphone-6-en-or-rose-15q2ZCDbQFU'}
+          className={vLegal_link}
+        >
+          {authorTexts.miaBakerLink[language]}
+        </Link>
+        <p>{authorTexts.lastParagraph[language]}</p>
       </section>
-    </>
+    </section>
   );
 }
